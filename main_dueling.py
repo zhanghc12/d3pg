db499a53e6359d2a6f732c3f63ed611d7929f018
zhanghc <zhanghc12@126.com>
2021-10-15 10:49:09 +0800

    dtpg

--- main_dueling.py
+++ main_dueling.py
@@ -7,6 +7,8 @@ import os
 import duelingpg.utils as utils
 from duelingpg import d3pg
 from duelingpg import OurDDPG
+from duelingpg import dtpg
+
 import datetime
 from torch.utils.tensorboard import SummaryWriter
 
@@ -36,7 +38,7 @@ def eval_policy(policy, env_name, seed, eval_episodes=10):
 if __name__ == "__main__":
 
     parser = argparse.ArgumentParser()
-    parser.add_argument("--policy", default="OurDDPG")  # Policy name (TD3, DDPG or OurDDPG, Dueling)
+    parser.add_argument("--policy", default="DTPG")  # Policy name (TD3, DDPG or OurDDPG, Dueling)
     parser.add_argument("--env", default="HalfCheetah-v2")  # OpenAI gym environment name
     parser.add_argument("--seed", default=0, type=int)  # Sets Gym, PyTorch and Numpy seeds
     parser.add_argument("--start_timesteps", default=25e3, type=int)  # Time steps initial random policy is used
@@ -51,6 +53,7 @@ if __name__ == "__main__":
     parser.add_argument("--policy_freq", default=2, type=int)  # Frequency of delayed policy updates
     parser.add_argument("--save_model", action="store_true")  # Save model and optimizer parameters
     parser.add_argument("--load_model", default="")  # Model load file name, "" doesn't load, "default" uses file_name
+    parser.add_argument("--version", default=1, type=int)
 
     args = parser.parse_args()
 
@@ -59,7 +62,7 @@ if __name__ == "__main__":
     else:
         experiment_dir = '/tmp/data/zhanghc/d3pg/'
     writer = SummaryWriter(
-        experiment_dir + '{}_{}_{}_{}'.format(datetime.datetime.now().strftime("%Y-%m-%d_%H-%M-%S"), args.policy, args.env, args.seed))
+        experiment_dir + '{}_{}_{}_s{}_ver{}'.format(datetime.datetime.now().strftime("%Y-%m-%d_%H-%M-%S"), args.policy, args.env, args.seed, args.version))
 
     file_name = f"{args.policy}_{args.env}_{args.seed}"
     print("---------------------------------------")
@@ -98,6 +101,10 @@ if __name__ == "__main__":
         #kwargs["noise_clip"] = args.noise_clip * max_action
         #kwargs["policy_freq"] = args.policy_freq
         policy = d3pg.D3PG(**kwargs)
+    elif args.policy == 'DTPG':
+        kwargs['version'] = args.version
+        policy = dtpg.DTPG(**kwargs)
+
     elif args.policy == "TD3":
         # Target policy smoothing is scaled wrt the action scale
         kwargs["policy_noise"] = args.policy_noise * max_action
@@ -154,8 +161,8 @@ if __name__ == "__main__":
             writer.add_scalar('loss/adv_loss', adv_loss, t)
         if done:
             # +1 to account for 0 indexing. +0 on ep_timesteps since it will increment +1 even if done=True
-            print(
-                f"Total T: {t + 1} Episode Num: {episode_num + 1} Episode T: {episode_timesteps} Reward: {episode_reward:.3f}")
+            #print(
+            #    f"Total T: {t + 1} Episode Num: {episode_num + 1} Episode T: {episode_timesteps} Reward: {episode_reward:.3f}")
             # Reset environment
             writer.add_scalar('train/return', episode_reward, t)
 
