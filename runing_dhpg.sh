4b31a3f0e43e562e065976dfe712be52ceab4b08
zhanghc <zhanghc12@126.com>
2021-10-18 11:35:34 +0800

    dhpg

--- runing_dhpg.sh
+++ runing_dhpg.sh
@@ -1,8 +1,4 @@
 CUDA_VISIBLE_DEVICES=0 python main_dueling.py --policy DHPG  --env HalfCheetah-v3 --seed 0 &\
-CUDA_VISIBLE_DEVICES=0 python main_dueling.py --policy DHPG  --env HalfCheetah-v3 --seed 1 &\
 CUDA_VISIBLE_DEVICES=1 python main_dueling.py --policy DHPG  --env HalfCheetah-v3 --seed 0 &\
-CUDA_VISIBLE_DEVICES=1 python main_dueling.py --policy DHPG  --env HalfCheetah-v3 --seed 1 &\
 CUDA_VISIBLE_DEVICES=2 python main_dueling.py --policy DHPG  --env Hopper-v3 --seed 0 &\
-CUDA_VISIBLE_DEVICES=2 python main_dueling.py --policy DHPG  --env Hopper-v3 --seed 1 &\
-CUDA_VISIBLE_DEVICES=3 python main_dueling.py --policy DHPG  --env Hopper-v3 --seed 0 &\
-CUDA_VISIBLE_DEVICES=3 python main_dueling.py --policy DHPG  --env Hopper-v3 --seed 1
+CUDA_VISIBLE_DEVICES=3 python main_dueling.py --policy DHPG  --env Hopper-v3 --seed 0
