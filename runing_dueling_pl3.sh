3a32741e5f79938901581d32833885aeafb54de8
zhanghc <zhanghc12@126.com>
2021-10-25 17:16:02 +0800

    12

---
+++ runing_dueling_pl3.sh
@@ -0,0 +1,8 @@
+CUDA_VISIBLE_DEVICES=0 python main_dueling.py --policy Dueling  --version 0 --env HalfCheetah-v3 --seed 0 &\
+CUDA_VISIBLE_DEVICES=0 python main_dueling.py --policy Dueling   --version 0 --env HalfCheetah-v3 --seed 1 &\
+CUDA_VISIBLE_DEVICES=0 python main_dueling.py --policy Dueling  --version 1  --env HalfCheetah-v3 --seed 0 &\
+CUDA_VISIBLE_DEVICES=0 python main_dueling.py --policy Dueling  --version 1  --env HalfCheetah-v3 --seed 1 &\
+CUDA_VISIBLE_DEVICES=1 python main_dueling.py --policy Dueling  --version 0  --env Hopper-v3 --seed 0 &\
+CUDA_VISIBLE_DEVICES=1 python main_dueling.py --policy Dueling   --version 0 --env Hopper-v3 --seed 1 &\
+CUDA_VISIBLE_DEVICES=1 python main_dueling.py --policy Dueling  --version 1  --env Hopper-v3 --seed 0 &\
+CUDA_VISIBLE_DEVICES=1 python main_dueling.py --policy Dueling  --version 1  --env Hopper-v3 --seed 1
