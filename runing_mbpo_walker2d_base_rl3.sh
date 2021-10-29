d9a95ebb487154e3d01d3ead28422c2d73b86c7e
zhanghc <zhanghc12@126.com>
2021-10-29 10:39:11 +0800

    1

---
+++ runing_mbpo_walker2d_base_rl3.sh
@@ -0,0 +1,6 @@
+CUDA_VISIBLE_DEVICES=0 python main_mbpo.py --model_type pytorch --version 10 --seed 0 --env_name Walker2d-v2 &\
+CUDA_VISIBLE_DEVICES=1 python main_mbpo.py --model_type pytorch --version 10 --seed 1 --env_name Walker2d-v2 &\
+CUDA_VISIBLE_DEVICES=2 python main_mbpo.py --model_type tensorflow --version 10 --seed 0 --env_name Walker2d-v2 &\
+CUDA_VISIBLE_DEVICES=3 python main_mbpo.py --model_type tensorflow --version 10 --seed 1 --env_name Walker2d-v2
+
+
