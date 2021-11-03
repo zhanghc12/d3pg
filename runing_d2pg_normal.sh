c780ef640286f57122f40b7820b2fefd18fec051
zhanghc <zhanghc12@126.com>
2021-11-03 16:58:39 +0800

    1

--- runing_d2pg_normal.sh
+++ runing_d2pg_normal.sh
@@ -1,8 +1,8 @@
-CUDA_VISIBLE_DEVICES=0 python main_dueling.py --policy d2pg --env Walker2d-v2 --seed 0 &\
-CUDA_VISIBLE_DEVICES=0 python main_dueling.py --policy d2pg --env Walker2d-v2 --seed 1 &\
-CUDA_VISIBLE_DEVICES=0 python main_dueling.py --policy d2pg --env Hopper-v2 --seed 0 &\
-CUDA_VISIBLE_DEVICES=0 python main_dueling.py --policy d2pg --env Hopper-v2 --seed 1 &\
-CUDA_VISIBLE_DEVICES=1 python main_dueling.py --policy d2pg --env HalfCheetah-v2 --seed 0 &\
-CUDA_VISIBLE_DEVICES=1 python main_dueling.py --policy d2pg --env HalfCheetah-v2 --seed 1 &\
-CUDA_VISIBLE_DEVICES=1 python main_dueling.py --policy d2pg --env Ant-v2 --seed 0 &\
-CUDA_VISIBLE_DEVICES=1 python main_dueling.py --policy d2pg --env Ant-v2 --seed 1
+CUDA_VISIBLE_DEVICES=0 python main_dueling.py --policy D2PG --env Walker2d-v2 --seed 0 &\
+CUDA_VISIBLE_DEVICES=0 python main_dueling.py --policy D2PG --env Walker2d-v2 --seed 1 &\
+CUDA_VISIBLE_DEVICES=0 python main_dueling.py --policy D2PG --env Hopper-v2 --seed 0 &\
+CUDA_VISIBLE_DEVICES=0 python main_dueling.py --policy D2PG --env Hopper-v2 --seed 1 &\
+CUDA_VISIBLE_DEVICES=1 python main_dueling.py --policy D2PG --env HalfCheetah-v2 --seed 0 &\
+CUDA_VISIBLE_DEVICES=1 python main_dueling.py --policy D2PG --env HalfCheetah-v2 --seed 1 &\
+CUDA_VISIBLE_DEVICES=1 python main_dueling.py --policy D2PG --env Ant-v2 --seed 0 &\
+CUDA_VISIBLE_DEVICES=1 python main_dueling.py --policy D2PG --env Ant-v2 --seed 1
--- runing_d6pg_normal.sh
+++ runing_d6pg_normal.sh
@@ -1,8 +1,8 @@
-CUDA_VISIBLE_DEVICES=0 python main_dueling.py --policy d6pg  --version $1  --target_threshold $2 --env Walker2d-v2 --seed 0 &\
-CUDA_VISIBLE_DEVICES=0 python main_dueling.py --policy d6pg  --version $1  --target_threshold $2  --env Walker2d-v2 --seed 1 &\
-CUDA_VISIBLE_DEVICES=0 python main_dueling.py --policy d6pg  --version $1  --target_threshold $2  --env Hopper-v2 --seed 0 &\
-CUDA_VISIBLE_DEVICES=0 python main_dueling.py --policy d6pg  --version $1  --target_threshold $2  --env Hopper-v2 --seed 1 &\
-CUDA_VISIBLE_DEVICES=1 python main_dueling.py --policy d6pg  --version $1  --target_threshold $2  --env HalfCheetah-v2 --seed 0 &\
-CUDA_VISIBLE_DEVICES=1 python main_dueling.py --policy d6pg  --version $1  --target_threshold $2  --env HalfCheetah-v2 --seed 1 &\
-CUDA_VISIBLE_DEVICES=1 python main_dueling.py --policy d6pg  --version $1  --target_threshold $2  --env Ant-v2 --seed 0 &\
-CUDA_VISIBLE_DEVICES=1 python main_dueling.py --policy d6pg  --version $1  --target_threshold $2  --env Ant-v2 --seed 1
+CUDA_VISIBLE_DEVICES=0 python main_dueling.py --policy D6PG  --version $1  --target_threshold $2 --env Walker2d-v2 --seed 0 &\
+CUDA_VISIBLE_DEVICES=0 python main_dueling.py --policy D6PG  --version $1  --target_threshold $2  --env Walker2d-v2 --seed 1 &\
+CUDA_VISIBLE_DEVICES=0 python main_dueling.py --policy D6PG  --version $1  --target_threshold $2  --env Hopper-v2 --seed 0 &\
+CUDA_VISIBLE_DEVICES=0 python main_dueling.py --policy D6PG  --version $1  --target_threshold $2  --env Hopper-v2 --seed 1 &\
+CUDA_VISIBLE_DEVICES=1 python main_dueling.py --policy D6PG  --version $1  --target_threshold $2  --env HalfCheetah-v2 --seed 0 &\
+CUDA_VISIBLE_DEVICES=1 python main_dueling.py --policy D6PG  --version $1  --target_threshold $2  --env HalfCheetah-v2 --seed 1 &\
+CUDA_VISIBLE_DEVICES=1 python main_dueling.py --policy D6PG  --version $1  --target_threshold $2  --env Ant-v2 --seed 0 &\
+CUDA_VISIBLE_DEVICES=1 python main_dueling.py --policy D6PG  --version $1  --target_threshold $2  --env Ant-v2 --seed 1
