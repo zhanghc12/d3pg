8b174c9042f6ac5cbc069ec41a8895d5e13a5ac5
zhanghc <zhanghc12@126.com>
2021-11-14 17:12:01 +0800

    1

--- runing_dvpg_normal.sh
+++ runing_dvpg_normal.sh
@@ -1,11 +1,11 @@
-CUDA_VISIBLE_DEVICES=0 python main_dueling.py --policy DVPG  --version $1  --tau $2 --env Walker2d-v2 --seed 0 &\
-CUDA_VISIBLE_DEVICES=0 python main_dueling.py --policy DVPG  --version $1  --tau $2  --env Walker2d-v2 --seed 1 &\
-CUDA_VISIBLE_DEVICES=1 python main_dueling.py --policy DVPG  --version $1  --tau $2  --env Hopper-v2 --seed 0 &\
-CUDA_VISIBLE_DEVICES=1 python main_dueling.py --policy DVPG  --version $1  --tau $2  --env Hopper-v2 --seed 1 &\
-CUDA_VISIBLE_DEVICES=2 python main_dueling.py --policy DVPG  --version $1  --tau $2  --env HalfCheetah-v2 --seed 0 &\
-CUDA_VISIBLE_DEVICES=2 python main_dueling.py --policy DVPG  --version $1  --tau $2  --env HalfCheetah-v2 --seed 1 &\
-CUDA_VISIBLE_DEVICES=3 python main_dueling.py --policy DVPG  --version $1  --tau $2  --env Ant-v2 --seed 0 &\
-CUDA_VISIBLE_DEVICES=3 python main_dueling.py --policy DVPG  --version $1  --tau $2  --env Ant-v2 --seed 1
+CUDA_VISIBLE_DEVICES=0 python main_dueling.py --policy DVPG  --version $1  --tau 0.1 --env Walker2d-v2 --seed 0 &\
+CUDA_VISIBLE_DEVICES=0 python main_dueling.py --policy DVPG  --version $1  --tau 0.1  --env Walker2d-v2 --seed 1 &\
+CUDA_VISIBLE_DEVICES=1 python main_dueling.py --policy DVPG  --version $1  --tau 0.1  --env Hopper-v2 --seed 0 &\
+CUDA_VISIBLE_DEVICES=1 python main_dueling.py --policy DVPG  --version $1  --tau 0.1  --env Hopper-v2 --seed 1 &\
+CUDA_VISIBLE_DEVICES=2 python main_dueling.py --policy DVPG  --version $1  --tau 0.1  --env HalfCheetah-v2 --seed 0 &\
+CUDA_VISIBLE_DEVICES=2 python main_dueling.py --policy DVPG  --version $1  --tau 0.1  --env HalfCheetah-v2 --seed 1 &\
+CUDA_VISIBLE_DEVICES=3 python main_dueling.py --policy DVPG  --version $1  --tau 0.1  --env Ant-v2 --seed 0 &\
+CUDA_VISIBLE_DEVICES=3 python main_dueling.py --policy DVPG  --version $1  --tau 0.1  --env Ant-v2 --seed 1
 
 
 # CUDA_VISIBLE_DEVICES=1 python main_dueling.py --policy D4PG  --version 2  --target_threshold 0  --env Ant-v2 --seed 1
