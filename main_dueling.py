eaa42a39296eaeda88f0ed1dfcb625ed0ffa32c9
zhanghc <zhanghc12@126.com>
2021-11-14 17:13:40 +0800

    1

--- main_dueling.py
+++ main_dueling.py
@@ -54,7 +54,7 @@ if __name__ == "__main__":
     parser.add_argument("--expl_noise", default=0.1)  # Std of Gaussian exploration noise
     parser.add_argument("--batch_size", default=256, type=int)  # Batch size for both actor and critic
     parser.add_argument("--discount", default=0.99)  # Discount factor
-    parser.add_argument("--tau", default=0.005)  # Target network update rate
+    parser.add_argument("--tau", default=0.005, type=float)  # Target network update rate
     parser.add_argument("--policy_noise", default=0.2)  # Noise added to target policy during critic update
     parser.add_argument("--noise_clip", default=0.5)  # Range to clip target policy noise
     parser.add_argument("--policy_freq", default=2, type=int)  # Frequency of delayed policy updates
--- runing_dvpg_normal.sh
+++ runing_dvpg_normal.sh
@@ -8,4 +8,4 @@ CUDA_VISIBLE_DEVICES=3 python main_dueling.py --policy DVPG  --version $1  --tau
 CUDA_VISIBLE_DEVICES=3 python main_dueling.py --policy DVPG  --version $1  --tau 0.1  --env Ant-v2 --seed 1
 
 
-# CUDA_VISIBLE_DEVICES=1 python main_dueling.py --policy D4PG  --version 2  --target_threshold 0  --env Ant-v2 --seed 1
+# CUDA_VISIBLE_DEVICES=1 python main_dueling.py --policy DVPG  --version 2  --tau 0.1  --env Hopper-v2 --seed 1
