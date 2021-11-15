41af5d7b008646392c365f2fecda53a004eb606a
zhanghc <zhanghc12@126.com>
2021-11-15 12:02:29 +0800

    1

--- runing_dvpg_normal.sh
+++ runing_dvpg_normal.sh
@@ -1,11 +1,11 @@
-CUDA_VISIBLE_DEVICES=0 python main_dueling.py --policy DVPG  --version $1  --tau 0.1 --env Walker2d-v2 --seed 0 &\
-CUDA_VISIBLE_DEVICES=0 python main_dueling.py --policy DVPG  --version $1  --tau 0.1  --env Walker2d-v2 --seed 1 &\
-CUDA_VISIBLE_DEVICES=1 python main_dueling.py --policy DVPG  --version $1  --tau 0.1  --env Hopper-v2 --seed 0 &\
-CUDA_VISIBLE_DEVICES=1 python main_dueling.py --policy DVPG  --version $1  --tau 0.1  --env Hopper-v2 --seed 1 &\
-CUDA_VISIBLE_DEVICES=2 python main_dueling.py --policy DVPG  --version $1  --tau 0.1  --env HalfCheetah-v2 --seed 0 &\
-CUDA_VISIBLE_DEVICES=2 python main_dueling.py --policy DVPG  --version $1  --tau 0.1  --env HalfCheetah-v2 --seed 1 &\
-CUDA_VISIBLE_DEVICES=3 python main_dueling.py --policy DVPG  --version $1  --tau 0.1  --env Ant-v2 --seed 0 &\
-CUDA_VISIBLE_DEVICES=3 python main_dueling.py --policy DVPG  --version $1  --tau 0.1  --env Ant-v2 --seed 1
+CUDA_VISIBLE_DEVICES=0 python main_dueling.py --policy DVPG  --version $1  --tau 0.03 --env Walker2d-v2 --seed 0 &\
+CUDA_VISIBLE_DEVICES=0 python main_dueling.py --policy DVPG  --version $1  --tau 0.03  --env Walker2d-v2 --seed 1 &\
+CUDA_VISIBLE_DEVICES=1 python main_dueling.py --policy DVPG  --version $1  --tau 0.03  --env Hopper-v2 --seed 0 &\
+CUDA_VISIBLE_DEVICES=1 python main_dueling.py --policy DVPG  --version $1  --tau 0.03  --env Hopper-v2 --seed 1 &\
+CUDA_VISIBLE_DEVICES=2 python main_dueling.py --policy DVPG  --version $1  --tau 0.03 --env HalfCheetah-v2 --seed 0 &\
+CUDA_VISIBLE_DEVICES=2 python main_dueling.py --policy DVPG  --version $1  --tau 0.03 --env HalfCheetah-v2 --seed 1 &\
+CUDA_VISIBLE_DEVICES=3 python main_dueling.py --policy DVPG  --version $1  --tau 0.03 --env Ant-v2 --seed 0 &\
+CUDA_VISIBLE_DEVICES=3 python main_dueling.py --policy DVPG  --version $1  --tau 0.03 --env Ant-v2 --seed 1
 
 
 # CUDA_VISIBLE_DEVICES=1 python main_dueling.py --policy DVPG  --version 2  --tau 0.1  --env Hopper-v2 --seed 1
