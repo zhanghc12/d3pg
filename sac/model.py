1064880b318b9bc2f022c477b32eb36c43a849e3
zhanghc <zhanghc12@126.com>
2021-11-15 17:36:48 +0800

    1

---
+++ sac/model.py
@@ -0,0 +1,227 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from torch.distributions import Normal
+
+LOG_SIG_MAX = 2
+LOG_SIG_MIN = -20
+epsilon = 1e-6
+
+
+# Initialize Policy weights
+def weights_init_(m):
+    if isinstance(m, nn.Linear):
+        torch.nn.init.xavier_uniform_(m.weight, gain=1)
+        torch.nn.init.constant_(m.bias, 0)
+
+
+class ValueNetwork(nn.Module):
+    def __init__(self, num_inputs, hidden_dim):
+        super(ValueNetwork, self).__init__()
+
+        self.linear1 = nn.Linear(num_inputs, hidden_dim)
+        self.linear2 = nn.Linear(hidden_dim, hidden_dim)
+        self.linear3 = nn.Linear(hidden_dim, 1)
+
+        self.apply(weights_init_)
+
+    def forward(self, state):
+        x = F.relu(self.linear1(state))
+        x = F.relu(self.linear2(x))
+        x = self.linear3(x)
+        return x
+
+
+class QNetwork(nn.Module):
+    def __init__(self, num_inputs, num_actions, hidden_dim):
+        super(QNetwork, self).__init__()
+
+        # Q1 architecture
+        self.linear1 = nn.Linear(num_inputs + num_actions, hidden_dim)
+        self.linear2 = nn.Linear(hidden_dim, hidden_dim)
+        self.linear3 = nn.Linear(hidden_dim, 1)
+
+        # Q2 architecture
+        self.linear4 = nn.Linear(num_inputs + num_actions, hidden_dim)
+        self.linear5 = nn.Linear(hidden_dim, hidden_dim)
+        self.linear6 = nn.Linear(hidden_dim, 1)
+
+        self.apply(weights_init_)
+
+    def forward(self, state, action):
+        xu = torch.cat([state, action], 1)
+
+        x1 = F.relu(self.linear1(xu))
+        x1 = F.relu(self.linear2(x1))
+        x1 = self.linear3(x1)
+
+        x2 = F.relu(self.linear4(xu))
+        x2 = F.relu(self.linear5(x2))
+        x2 = self.linear6(x2)
+
+        return x1, x2
+
+
+
+class DuelingNetwork(nn.Module):
+    def __init__(self, num_inputs, num_actions, hidden_dim):
+        super(DuelingNetwork, self).__init__()
+
+        self.l1 = nn.Linear(num_inputs, 256)
+        self.l2 = nn.Linear(256, 256)
+        self.lv_1 = nn.Linear(256, 1)
+
+        self.l3 = nn.Linear(256 + num_actions, 256)
+        self.la_1 = nn.Linear(256, 1)
+
+        self.l4 = nn.Linear(num_inputs, 256)
+        self.l5 = nn.Linear(256, 256)
+        self.lv_2 = nn.Linear(256, 1)
+
+        self.l6 = nn.Linear(256 + num_actions, 256)
+        self.la_2 = nn.Linear(256, 1)
+
+        self.apply(weights_init_)
+
+    def forward(self, state, action, return_full=False):
+        feat = F.relu(self.l2(F.relu(self.l1(state))))
+        value_1 = self.lv_1(feat)
+        adv_1 = F.relu(self.l3(torch.cat([feat, action], 1)))
+        adv_1 = self.la_1(adv_1)
+        q1 = adv_1 + value_1
+
+        feat = F.relu(self.l5(F.relu(self.l4(state))))
+        value_2 = self.lv_2(feat)
+        adv_2 = F.relu(self.l6(torch.cat([feat, action], 1)))
+        adv_2 = self.la_2(adv_2)
+        q2 = adv_2 + value_2
+
+        if return_full:
+            return value_1, adv_1, q1, value_2, adv_2, q2
+        return q1, q2
+
+    def get_value(self, state):
+        feat = F.relu(self.l2(F.relu(self.l1(state))))
+        value_1 = self.lv_1(feat)
+
+        feat = F.relu(self.l5(F.relu(self.l4(state))))
+        value_2 = self.lv_2(feat)
+
+        return value_1, value_2
+
+    def get_adv(self, state, action):
+        feat = F.relu(self.l2(F.relu(self.l1(state))))
+        adv_1 = F.relu(self.l3(torch.cat([feat, action], 1)))
+        adv_1 = self.la_1(adv_1)
+
+        feat = F.relu(self.l5(F.relu(self.l4(state))))
+        adv_2 = F.relu(self.l6(torch.cat([feat, action], 1)))
+        adv_2 = self.la_2(adv_2)
+
+        return adv_1, adv_2
+
+class GaussianPolicy(nn.Module):
+    def __init__(self, num_inputs, num_actions, hidden_dim, action_space=None):
+        super(GaussianPolicy, self).__init__()
+
+        self.linear1 = nn.Linear(num_inputs, hidden_dim)
+        self.linear2 = nn.Linear(hidden_dim, hidden_dim)
+
+        self.mean_linear = nn.Linear(hidden_dim, num_actions)
+        self.log_std_linear = nn.Linear(hidden_dim, num_actions)
+
+        self.apply(weights_init_)
+
+        # action rescaling
+        if action_space is None:
+            self.action_scale = torch.tensor(1.)
+            self.action_bias = torch.tensor(0.)
+        else:
+            self.action_scale = torch.FloatTensor(
+                (action_space.high - action_space.low) / 2.)
+            self.action_bias = torch.FloatTensor(
+                (action_space.high + action_space.low) / 2.)
+
+    def forward(self, state):
+        x = F.relu(self.linear1(state))
+        x = F.relu(self.linear2(x))
+        mean = self.mean_linear(x)
+        log_std = self.log_std_linear(x)
+        log_std = torch.clamp(log_std, min=LOG_SIG_MIN, max=LOG_SIG_MAX)
+        return mean, log_std
+
+    def sample(self, state):
+        mean, log_std = self.forward(state)
+        std = log_std.exp()
+        normal = Normal(mean, std)
+        x_t = normal.rsample()  # for reparameterization trick (mean + std * N(0,1))
+        y_t = torch.tanh(x_t)
+        action = y_t * self.action_scale + self.action_bias
+        log_prob = normal.log_prob(x_t)
+        # Enforcing Action Bound
+        log_prob -= torch.log(self.action_scale * (1 - y_t.pow(2)) + epsilon)
+        log_prob = log_prob.sum(1, keepdim=True)
+        mean = torch.tanh(mean) * self.action_scale + self.action_bias
+        return action, log_prob, mean
+
+    def to(self, device):
+        self.action_scale = self.action_scale.to(device)
+        self.action_bias = self.action_bias.to(device)
+        return super(GaussianPolicy, self).to(device)
+
+
+    def log_prob(self, state, action):
+        mean, log_std = self.forward(state)
+        std = log_std.exp()
+        normal = Normal(mean, std)
+        # note: modified by zhc @0425
+        rescaled_action = (action - self.action_bias) / self.action_scale
+        u = 0.5 * torch.log((1 + epsilon + rescaled_action) / (1 + epsilon - rescaled_action))
+        # before tanh
+
+        log_prob = normal.log_prob(u)
+        # Enforcing Action Bound
+        log_prob -= torch.log(self.action_scale * (1 - rescaled_action.pow(2)) + epsilon)
+        log_prob = log_prob.sum(1, keepdim=True)
+
+        return log_prob
+
+class DeterministicPolicy(nn.Module):
+    def __init__(self, num_inputs, num_actions, hidden_dim, action_space=None):
+        super(DeterministicPolicy, self).__init__()
+        self.linear1 = nn.Linear(num_inputs, hidden_dim)
+        self.linear2 = nn.Linear(hidden_dim, hidden_dim)
+
+        self.mean = nn.Linear(hidden_dim, num_actions)
+        self.noise = torch.Tensor(num_actions)
+
+        self.apply(weights_init_)
+
+        # action rescaling
+        if action_space is None:
+            self.action_scale = 1.
+            self.action_bias = 0.
+        else:
+            self.action_scale = torch.FloatTensor(
+                (action_space.high - action_space.low) / 2.)
+            self.action_bias = torch.FloatTensor(
+                (action_space.high + action_space.low) / 2.)
+
+    def forward(self, state):
+        x = F.relu(self.linear1(state))
+        x = F.relu(self.linear2(x))
+        mean = torch.tanh(self.mean(x)) * self.action_scale + self.action_bias
+        return mean
+
+    def sample(self, state):
+        mean = self.forward(state)
+        noise = self.noise.normal_(0., std=0.1)
+        noise = noise.clamp(-0.25, 0.25)
+        action = mean + noise
+        return action, torch.tensor(0.), mean
+
+    def to(self, device):
+        self.action_scale = self.action_scale.to(device)
+        self.action_bias = self.action_bias.to(device)
+        self.noise = self.noise.to(device)
+        return super(DeterministicPolicy, self).to(device)
---
+++ sac/sac_dueling.py
@@ -0,0 +1,186 @@
+import os
+import torch
+import torch.nn.functional as F
+from torch.optim import Adam
+from sac.utils import soft_update, hard_update
+from sac.model import GaussianPolicy, QNetwork, DeterministicPolicy, DuelingNetwork
+
+
+class DuelingSAC(object):
+    def __init__(self, num_inputs, action_space, args):
+
+        self.gamma = args.gamma
+        self.tau = args.tau
+        self.alpha = args.alpha
+
+        self.policy_type = args.policy
+        self.target_update_interval = args.target_update_interval
+        self.automatic_entropy_tuning = args.automatic_entropy_tuning
+
+        self.device = torch.device("cuda" if args.cuda else "cpu")
+
+        self.version = args.version
+        self.critic = DuelingNetwork(num_inputs, action_space.shape[0], args.hidden_size).to(device=self.device)
+
+        self.critic_optim = Adam(self.critic.parameters(), lr=args.lr)
+
+        self.critic_target = DuelingNetwork(num_inputs, action_space.shape[0], args.hidden_size).to(self.device)
+        hard_update(self.critic_target, self.critic)
+
+        if self.policy_type == "Gaussian":
+            # Target Entropy = −dim(A) (e.g. , -6 for HalfCheetah-v2) as given in the paper
+            if self.automatic_entropy_tuning is True:
+                self.target_entropy = -torch.prod(torch.Tensor(action_space.shape).to(self.device)).item()
+                self.log_alpha = torch.zeros(1, requires_grad=True, device=self.device)
+                self.alpha_optim = Adam([self.log_alpha], lr=args.lr)
+
+            self.policy = GaussianPolicy(num_inputs, action_space.shape[0], args.hidden_size, action_space).to(self.device)
+            self.policy_optim = Adam(self.policy.parameters(), lr=args.lr)
+
+        else:
+            self.alpha = 0
+            self.automatic_entropy_tuning = False
+            self.policy = DeterministicPolicy(num_inputs, action_space.shape[0], args.hidden_size, action_space).to(self.device)
+            self.policy_optim = Adam(self.policy.parameters(), lr=args.lr)
+
+        self.behavior_policy = GaussianPolicy(num_inputs, action_space.shape[0], args.hidden_size, action_space).to(self.device)
+        self.behavior_policy_optim = Adam(self.behavior_policy.parameters(), lr=args.lr)
+
+
+    def select_action(self, state, evaluate=False):
+        state = torch.FloatTensor(state).to(self.device).unsqueeze(0)
+        if evaluate is False:
+            action, _, _ = self.policy.sample(state)
+        else:
+            _, _, action = self.policy.sample(state)
+        return action.detach().cpu().numpy()[0]
+
+    def update_parameters(self, memory, batch_size, updates):
+        # Sample a batch from memory
+        state_batch, action_batch, reward_batch, next_state_batch, mask_batch = memory.sample(batch_size=batch_size)
+
+        state_batch = torch.FloatTensor(state_batch).to(self.device)
+        next_state_batch = torch.FloatTensor(next_state_batch).to(self.device)
+        action_batch = torch.FloatTensor(action_batch).to(self.device)
+        reward_batch = torch.FloatTensor(reward_batch).to(self.device).unsqueeze(1)
+        mask_batch = torch.FloatTensor(mask_batch).to(self.device).unsqueeze(1)
+
+        '''
+        update Q function
+        '''
+        with torch.no_grad():
+            vf1_next_target, vf2_next_target = self.critic_target.get_value(next_state_batch)
+            min_vf_next_target = torch.min(vf1_next_target, vf2_next_target)
+            next_q_value = reward_batch + mask_batch * self.gamma * (min_vf_next_target)
+
+        value_1, adv_1, qf1, value_2, adv_2, qf2 = self.critic(state_batch, action_batch, return_full=True)
+
+        if self.version == 1:
+            _, _, pi = self.policy.sample(state_batch)
+            adv_pi_1, adv_pi_2 = self.critic.get_adv(state_batch, pi)
+        if self.version == 2:
+            self.num_repeat = 10
+            state_bath_temp = state_batch.unsqueeze(1).repeat(1, self.num_repeat, 1).view(state_batch.shape[0] * self.num_repeat, state_batch.shape[1])
+            pi_temp, _, _ = self.policy.sample(state_bath_temp)
+            adv_pi_1, adv_pi_2 = self.critic.get_adv(state_bath_temp, pi_temp)
+            adv_pi_1 = adv_pi_1.view(state_batch.shape[0], self.num_repeat, 1)
+            adv_pi_1 = adv_pi_1.mean(dim=1)
+            adv_pi_2 = adv_pi_2.view(state_batch.shape[0], self.num_repeat, 1)
+            adv_pi_2 = adv_pi_2.mean(dim=1)
+
+        # todo: enumerate all the samples
+        log_prob = self.policy.log_prob(state_batch, action_batch)
+
+        qf1_loss = F.mse_loss(qf1 - adv_pi_1 + self.alpha * log_prob, next_q_value)  # JQ = 𝔼(st,at)~D[0.5(Q1(st,at) - r(st,at) - γ(𝔼st+1~p[V(st+1)]))^2]
+        qf2_loss = F.mse_loss(qf2 - adv_pi_2 + self.alpha * log_prob, next_q_value)  # JQ = 𝔼(st,at)~D[0.5(Q1(st,at) - r(st,at) - γ(𝔼st+1~p[V(st+1)]))^2]
+        qf_loss = qf1_loss + qf2_loss
+        '''
+        update value function
+        '''
+        # v_t = E_pi(r + gamma V(s_t+1))
+        if self.version == 3:
+            with torch.no_grad():
+                behavior_log_prob = self.behavior_policy.log_prob(state_batch, action_batch)
+                log_prob = self.policy.log_prob(state_batch, action_batch)
+                vf1_next_target, vf2_next_target = self.critic_target.get_vf(next_state_batch)
+                min_vf_next_target = torch.min(vf1_next_target, vf2_next_target)
+                next_v = reward_batch + mask_batch * self.gamma * min_vf_next_target - self.alpha * log_prob  # todo, we need to get entroph here
+                importance_ratio = log_prob.exp() / behavior_log_prob.exp()
+                normalized_importance_ratio = importance_ratio / importance_ratio.sum()
+                next_v = normalized_importance_ratio * next_v
+
+            vf1, vf2 = self.critic.get_vf(state_batch)
+
+            vf1_loss = F.mse_loss(vf1, next_v)
+            vf2_loss = F.mse_loss(vf2, next_v)
+            vf_loss = vf1_loss + vf2_loss
+            qf_loss = vf_loss + qf_loss
+
+        self.critic_optim.zero_grad()
+        qf_loss.backward()
+        self.critic_optim.step()
+
+        '''
+        update behavior policy
+        '''
+        if self.version == 3:
+            behavior_log_prob = self.behavior_policy.log_prob(state_batch, action_batch)
+            behavior_policy_loss = -behavior_log_prob
+            self.behavior_policy_optim.zero_grad()
+            behavior_policy_loss.backward()
+            self.behavior_policy_optim.step()
+
+        '''
+        update actor
+        '''
+        pi, log_pi, _ = self.policy.sample(state_batch)
+
+        qf1_pi, qf2_pi = self.critic(state_batch, pi)
+        min_qf_pi = torch.min(qf1_pi, qf2_pi)
+
+        policy_loss = ((self.alpha * log_pi) - min_qf_pi).mean() # Jπ = 𝔼st∼D,εt∼N[α * logπ(f(εt;st)|st) − Q(st,f(εt;st))]
+
+        self.policy_optim.zero_grad()
+        policy_loss.backward()
+        self.policy_optim.step()
+
+        if self.automatic_entropy_tuning:
+            alpha_loss = -(self.log_alpha * (log_pi + self.target_entropy).detach()).mean()
+
+            self.alpha_optim.zero_grad()
+            alpha_loss.backward()
+            self.alpha_optim.step()
+
+            self.alpha = self.log_alpha.exp()
+            alpha_tlogs = self.alpha.clone() # For TensorboardX logs
+        else:
+            alpha_loss = torch.tensor(0.).to(self.device)
+            alpha_tlogs = torch.tensor(self.alpha) # For TensorboardX logs
+
+
+        if updates % self.target_update_interval == 0:
+            soft_update(self.critic_target, self.critic, self.tau)
+
+        return qf1_loss.item(), qf2_loss.item(), policy_loss.item(), alpha_loss.item(), alpha_tlogs.item()
+
+    # Save model parameters
+    def save_model(self, env_name, suffix="", actor_path=None, critic_path=None):
+        if not os.path.exists('models/'):
+            os.makedirs('models/')
+
+        if actor_path is None:
+            actor_path = "models/sac_actor_{}_{}".format(env_name, suffix)
+        if critic_path is None:
+            critic_path = "models/sac_critic_{}_{}".format(env_name, suffix)
+        print('Saving models to {} and {}'.format(actor_path, critic_path))
+        torch.save(self.policy.state_dict(), actor_path)
+        torch.save(self.critic.state_dict(), critic_path)
+
+    # Load model parameters
+    def load_model(self, actor_path, critic_path):
+        print('Loading models from {} and {}'.format(actor_path, critic_path))
+        if actor_path is not None:
+            self.policy.load_state_dict(torch.load(actor_path))
+        if critic_path is not None:
+            self.critic.load_state_dict(torch.load(critic_path))
+
