0bebc0d43523a9e2836f6c4ec74fe451b4497609
zhanghc <zhanghc12@126.com>
2021-11-15 11:36:37 +0800

    1

---
+++ runing_d4pg_baseline.sh
@@ -0,0 +1,11 @@
+CUDA_VISIBLE_DEVICES=0 python main_dueling.py --policy D4PG  --version $1  --target_threshold $2 --env Walker2d-v2 --seed $3 &\
+CUDA_VISIBLE_DEVICES=0 python main_dueling.py --policy D4PG  --version $1  --target_threshold $2  --env Walker2d-v2 --seed $3 &\
+CUDA_VISIBLE_DEVICES=0 python main_dueling.py --policy D4PG  --version $1  --target_threshold $2  --env Hopper-v2 --seed $3 &\
+CUDA_VISIBLE_DEVICES=0 python main_dueling.py --policy D4PG  --version $1  --target_threshold $2  --env Hopper-v2 --seed $3 &\
+CUDA_VISIBLE_DEVICES=1 python main_dueling.py --policy D4PG  --version $1  --target_threshold $2  --env HalfCheetah-v2 --seed $4 &\
+CUDA_VISIBLE_DEVICES=1 python main_dueling.py --policy D4PG  --version $1  --target_threshold $2  --env HalfCheetah-v2 --seed $4 &\
+CUDA_VISIBLE_DEVICES=1 python main_dueling.py --policy D4PG  --version $1  --target_threshold $2  --env Ant-v2 --seed $4 &\
+CUDA_VISIBLE_DEVICES=1 python main_dueling.py --policy D4PG  --version $1  --target_threshold $2  --env Ant-v2 --seed $4
+
+
+# CUDA_VISIBLE_DEVICES=1 python main_dueling.py --policy D4PG  --version 6  --target_threshold 0.02  --env Ant-v2 --seed 1
