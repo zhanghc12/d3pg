58a6940b0f5ee7877af4c2ab8089824a1ed42130
zhanghc <zhanghc12@126.com>
2021-11-25 11:39:54 +0800

    1

--- runing_dvac_normal_4gpu.sh
+++ runing_dvac_normal_4gpu.sh
@@ -1,11 +1,11 @@
-CUDA_VISIBLE_DEVICES=0 python main_sac.py --cuda  --version $1 --model_version $2 ---target_version $3 -env Walker2d-v2 --seed 0 &\
-CUDA_VISIBLE_DEVICES=0 python main_sac.py --cuda --version $1 --model_version $2 ---target_version $3  --env Walker2d-v2 --seed 1 &\
-CUDA_VISIBLE_DEVICES=1 python main_sac.py --cuda --version $1 --model_version $2 ---target_version $3  --env Hopper-v2 --seed 0 &\
-CUDA_VISIBLE_DEVICES=1 python main_sac.py --cuda --version $1 --model_version $2 ---target_version $3  --env Hopper-v2 --seed 1 &\
-CUDA_VISIBLE_DEVICES=2 python main_sac.py --cuda --version $1 --model_version $2 ---target_version $3 --env HalfCheetah-v2 --seed 0 &\
-CUDA_VISIBLE_DEVICES=2 python main_sac.py --cuda --version $1 --model_version $2 ---target_version $3 --env HalfCheetah-v2 --seed 1 &\
-CUDA_VISIBLE_DEVICES=3 python main_sac.py --cuda --version $1 --model_version $2 ---target_version $3  --env Ant-v2 --seed 0 &\
-CUDA_VISIBLE_DEVICES=3 python main_sac.py --cuda --version $1 --model_version $2 ---target_version $3 --env Ant-v2 --seed 1
+CUDA_VISIBLE_DEVICES=0 python main_sac.py --cuda  --version $1 --model_version $2 --target_version $3 -env Walker2d-v2 --seed 0 &\
+CUDA_VISIBLE_DEVICES=0 python main_sac.py --cuda --version $1 --model_version $2 --target_version $3  --env Walker2d-v2 --seed 1 &\
+CUDA_VISIBLE_DEVICES=1 python main_sac.py --cuda --version $1 --model_version $2 --target_version $3  --env Hopper-v2 --seed 0 &\
+CUDA_VISIBLE_DEVICES=1 python main_sac.py --cuda --version $1 --model_version $2 --target_version $3  --env Hopper-v2 --seed 1 &\
+CUDA_VISIBLE_DEVICES=2 python main_sac.py --cuda --version $1 --model_version $2 --target_version $3 --env HalfCheetah-v2 --seed 0 &\
+CUDA_VISIBLE_DEVICES=2 python main_sac.py --cuda --version $1 --model_version $2 --target_version $3 --env HalfCheetah-v2 --seed 1 &\
+CUDA_VISIBLE_DEVICES=3 python main_sac.py --cuda --version $1 --model_version $2 --target_version $3  --env Ant-v2 --seed 0 &\
+CUDA_VISIBLE_DEVICES=3 python main_sac.py --cuda --version $1 --model_version $2 --target_version $3 --env Ant-v2 --seed 1
 
 
 # CUDA_VISIBLE_DEVICES=3 python main_sac.py --cuda --version 0  --env Ant-v2 --seed 1 --start_steps 1000
