2f527538aff25848920dad02f131e1893abeb973
zhanghc <zhanghc12@126.com>
2021-11-25 11:40:40 +0800

    1

--- runing_dvac_normal_4gpu.sh
+++ runing_dvac_normal_4gpu.sh
@@ -1,4 +1,4 @@
-CUDA_VISIBLE_DEVICES=0 python main_sac.py --cuda  --version $1 --model_version $2 --target_version $3 -env Walker2d-v2 --seed 0 &\
+CUDA_VISIBLE_DEVICES=0 python main_sac.py --cuda  --version $1 --model_version $2 --target_version $3 --env Walker2d-v2 --seed 0 &\
 CUDA_VISIBLE_DEVICES=0 python main_sac.py --cuda --version $1 --model_version $2 --target_version $3  --env Walker2d-v2 --seed 1 &\
 CUDA_VISIBLE_DEVICES=1 python main_sac.py --cuda --version $1 --model_version $2 --target_version $3  --env Hopper-v2 --seed 0 &\
 CUDA_VISIBLE_DEVICES=1 python main_sac.py --cuda --version $1 --model_version $2 --target_version $3  --env Hopper-v2 --seed 1 &\
