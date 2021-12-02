ac49e69086efe2abdae0cdfaed5de8c77658795f
zhanghc <zhanghc12@126.com>
2021-12-02 14:40:12 +0800

    1

--- offline_dueling.py
+++ offline_dueling.py
@@ -24,8 +24,8 @@ def load_hdf5(dataset, replay_buffer):
     replay_buffer.state = dataset['observations']
     replay_buffer.action = dataset['actions']
     replay_buffer.next_state = dataset['next_observations']
-    replay_buffer.reward = dataset['rewards']
-    replay_buffer.not_done = 1 - dataset['terminals']
+    replay_buffer.reward = np.expand_dims(np.squeeze(dataset['rewards']), 1)
+    replay_buffer.not_done = 1 - np.expand_dims(np.squeeze(dataset['terminals']), 1)
 
 
     replay_buffer.size = dataset['terminals'].shape[0]
