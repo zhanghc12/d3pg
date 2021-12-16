acd11c71487d55bb6ec371efcde281f2f7c16967
zhanghc <zhanghc12@126.com>
2021-12-16 11:20:47 +0800

    1

--- eval_runing_dvac_normal_2gpu.sh
+++ eval_runing_dvac_normal_2gpu.sh
@@ -1,4 +1,4 @@
-CUDA_VISIBLE_DEVICES=0 python main_dueling_eval.py --policy Dueling  --version $1  --target_threshold $2  --num_critic $3  --env Walker2d-v2 --seed 0 &\
-CUDA_VISIBLE_DEVICES=0 python main_dueling_eval.py --policy Dueling  --version $1  --target_threshold $2  --num_critic $3  --env Hopper-v2 --seed 0 &\
-CUDA_VISIBLE_DEVICES=1 python main_dueling_eval.py --policy Dueling  --version $1  --target_threshold $2  --num_critic $3  --env HalfCheetah-v2 --seed 0 &\
-CUDA_VISIBLE_DEVICES=1 python main_dueling_eval.py --policy Dueling  --version $1  --target_threshold $2  --num_critic $3  --env Ant-v2 --seed 0
+CUDA_VISIBLE_DEVICES=0 python main_dueling_eval.py --policy Dueling  --version $1  --target_threshold $2  --num_critic $3 --tau $4  --env Walker2d-v2 --seed 0 &\
+CUDA_VISIBLE_DEVICES=0 python main_dueling_eval.py --policy Dueling  --version $1  --target_threshold $2  --num_critic $3 --tau $4 --env Hopper-v2 --seed 0 &\
+CUDA_VISIBLE_DEVICES=1 python main_dueling_eval.py --policy Dueling  --version $1  --target_threshold $2  --num_critic $3 --tau $4 --env HalfCheetah-v2 --seed 0 &\
+CUDA_VISIBLE_DEVICES=1 python main_dueling_eval.py --policy Dueling  --version $1  --target_threshold $2  --num_critic $3 --tau $4 --env Ant-v2 --seed 0
