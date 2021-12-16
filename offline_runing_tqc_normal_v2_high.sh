dcd275ac10bd39c210a9fd08ba3649fc0a1692c9
zhanghc <zhanghc12@126.com>
2021-12-16 10:57:28 +0800

    1

--- offline_runing_tqc_normal_v2_high.sh
+++ offline_runing_tqc_normal_v2_high.sh
@@ -1,8 +1,7 @@
 CUDA_VISIBLE_DEVICES=0 python offline_tqc.py --policy TQC  --version $1  --top_quantiles_to_drop_per_net $2  --n_nets $3 --env hopper-medium-v2  --seed 1 &\
 CUDA_VISIBLE_DEVICES=1 python offline_tqc.py --policy TQC  --version $1  --top_quantiles_to_drop_per_net $2  --n_nets $3 --env hopper-medium-expert-v2  --seed 0 &\
 CUDA_VISIBLE_DEVICES=2 python offline_tqc.py --policy TQC  --version $1  --top_quantiles_to_drop_per_net $2  --n_nets $3 --env hopper-expert-v2 --seed 0 &\
-CUDA_VISIBLE_DEVICES=3 python offline_tqc.py --policy TQC  --version $1  --top_quantiles_to_drop_per_net $2 --n_nets $3  --env walker2d-medium-expert-v2  --seed 0 &\
-CUDA_VISIBLE_DEVICES=0 python offline_tqc.py --policy TQC  --version $1  --top_quantiles_to_drop_per_net $2 --n_nets $3  --env walker2d-expert-v2 --seed 0
+CUDA_VISIBLE_DEVICES=3 python offline_tqc.py --policy TQC  --version $1  --top_quantiles_to_drop_per_net $2  --n_nets $3  --env walker2d-expert-v2 --seed 0
 
 
 # sh offline_rl_large_scale_single_seed_new.sh 31  1.0 -1 0.2  0.05
