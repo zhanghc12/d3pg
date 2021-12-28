730d66a9778d950fe051e9753d1af40211b8c0de
zhanghc <zhanghc12@126.com>
2021-12-28 20:15:47 +0800

    1

--- duelingpg/d3pg_eval_ensemble_exp_aux_dist.py
+++ duelingpg/d3pg_eval_ensemble_exp_aux_dist.py
@@ -7,6 +7,7 @@ import torch.autograd as autograd
 from duelingpg import utils
 from torch.distributions import Normal
 
+
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 
@@ -22,6 +23,22 @@ class Actor(nn.Module):
 
     def forward(self, state):
         a = F.relu(self.l1(state))
+        a = F.relu(self.l2(a))
+        return self.max_action * torch.tanh(self.l3(a))
+
+
+class ExpActor(nn.Module):
+    def __init__(self, state_dim, action_dim, max_action):
+        super(ExpActor, self).__init__()
+
+        self.l1 = nn.Linear(state_dim + action_dim, 256)
+        self.l2 = nn.Linear(256, 256)
+        self.l3 = nn.Linear(256, action_dim)
+
+        self.max_action = max_action
+
+    def forward(self, state, action):
+        a = F.relu(self.l1(torch.cat([state, action], dim=1)))
         a = F.relu(self.l2(a))
         return self.max_action * torch.tanh(self.l3(a))
 
@@ -65,10 +82,14 @@ class LipRandomReward(nn.Module):
 
 
 class D3PG(object):
-    def __init__(self, state_dim, action_dim, max_action, discount=0.99, tau=0.005, version=0, target_threshold=0.1, num_critic=2, exp_version=0):
+    def __init__(self, state_dim, action_dim, max_action, discount=0.99, tau=0.005, version=0, target_threshold=0.1, num_critic=2, exp_version=0, exp_num_critic=2):
         self.actor = Actor(state_dim, action_dim, max_action).to(device)
         self.actor_target = copy.deepcopy(self.actor)
         self.actor_optimizer = torch.optim.Adam(self.actor.parameters(), lr=3e-4)
+
+        self.exp_actor = ExpActor(state_dim, action_dim, target_threshold).to(device)
+        self.exp_actor_optimizer = torch.optim.Adam(self.exp_actor.parameters(), lr=3e-4)
+
 
         '''
         define the ensemble critics
@@ -85,7 +106,6 @@ class D3PG(object):
         '''
         define the exploration critics
         '''
-        exp_num_critic = num_critic
         self.exp_num_critic = exp_num_critic
         self.exp_critics = nn.ModuleList()
         for i in range(self.exp_num_critic):
@@ -127,23 +147,41 @@ class D3PG(object):
         noise_scale = np.linalg.norm(noise)
         if self.version == 0:
             return noise
+        if self.target_threshold > 0:
+            return np.zeros_like(noise)
 
         exp_Qs = []
-        if self.version == 1:
+        exp_Qs_stds = []
+
+        if self.version in [1,3]:
             for i in range(self.exp_num_critic):
-                exp_Qs.append(self.exp_critics[i](state, action))
+                Q_mean, Q_log_std = self.exp_critics[i](state, action)
+                Q_std = torch.exp(Q_log_std)
+                exp_Qs.append(Q_mean)
+                exp_Qs_stds.append(Q_std)
         elif self.version == 2:
             for i in range(self.num_critic):
-                exp_Qs.append(self.critics[i](state, action))
+                Q_mean, Q_log_std = self.critics[i](state, action)
+                Q_std = torch.exp(Q_log_std)
+                exp_Qs.append(Q_mean)
+                exp_Qs_stds.append(Q_std)
+
         exp_Qs = torch.cat(exp_Qs, dim=1)
-        std_Q = torch.std(exp_Qs, dim=1).mean()
-        action_grad = autograd.grad(std_Q, action, retain_graph=True)[0]
+        exp_Qs_stds = torch.cat(exp_Qs_stds, dim=1)
+        mean_exp_Qs = torch.mean(exp_Qs, dim=1)
+        var_exp_Q = (torch.mean(exp_Qs ** 2, dim=1) + torch.mean(exp_Qs_stds, dim=1) - mean_exp_Qs ** 2).mean()
+
+        action_grad = autograd.grad(var_exp_Q, action, retain_graph=True)[0]
         action_grad = action_grad / action_grad.norm()
         return noise_scale * action_grad.cpu().data.numpy().flatten()
 
-    def select_action(self, state):
+    def select_action(self, state, noisy=True):
         state = torch.FloatTensor(state.reshape(1, -1)).to(device)
-        return self.actor(state).cpu().data.numpy().flatten()
+        if self.target_threshold > 0. and noisy == True:
+            action = self.actor(state)
+            return (action + self.exp_actor(state, action)).cpu().data.numpy().flatten()
+        else:
+            return self.actor(state).cpu().data.numpy().flatten()
 
     def train(self, replay_buffer, batch_size=256):
         self.total_it += 1
@@ -157,44 +195,20 @@ class D3PG(object):
         Compute critic loss
         '''
         # formulate target Q network
-        target_Qs_sample = []
+        target_Qs = []
         for i in range(self.num_critic):
-            target_Q_sample, target_Q_log_std, target_Q_mean = self.critics_target[i](next_state, self.actor_target(next_state))
-            target_Qs_sample.append(target_Q_sample)
-        target_Q = torch.min(torch.cat(target_Qs_sample, dim=1), dim=1, keepdim=True)[0]
+            target_Q, _ = self.critics_target[i](next_state, self.actor_target(next_state))
+            target_Qs.append(target_Q)
+        target_Q = torch.min(torch.cat(target_Qs, dim=1), dim=1, keepdim=True)[0]
         target_Q = reward + (not_done * self.discount * target_Q).detach()
 
         # get the critic loss
         critic_loss = 0.
         for i in range(self.num_critic):
-            current_Q_sample, current_Q_log_std, current_Q_mean  = self.critics[i](state, action)
-            critic_loss += F.mse_loss(current_Q_sample, target_Q)
-
-        target_means = []
-        target_stds = []
-        for i in range(self.num_critic):
-            target_mean, target_log_std = self.critics_target[i](next_state, self.actor_target(next_state)) # get mean and std
-            target_means.append(target_mean)
-            target_stds.append(target_log_std.exp())
-
-        means = []
-        stds = []
-        for i in range(self.num_critic):
-            mean, log_std = self.critics[i](state, action) # get mean and std
-            means.append(mean)
-            stds.append(log_std.exp())
-
-        # then cal target
-        for i in range(self.num_critic):
-            # target_Q = torch.min(torch.cat(target_Qs_sample, dim=1), dim=1, keepdim=True)[0]
-            target_means[i] = reward + (not_done * self.discount * target_means[i])
-            target_stds[i] = not_done * self.discount * target_stds[i]
-            target_means[i] = target_means[i].detach()
-            target_stds[i] = target_stds[i].detach()
-
-        # then cal kl
-        for i in range(self.num_critic):
-            critic_loss += -2 * torch.log(stds[i] + 1e-5) + (stds[i] ** 2) / (target_stds[i] ** 2 + 1e-5) + ((means[i] - target_means[i]) ** 2) / (target_stds[i] ** 2 + 1e-5)
+            current_Q_mean, current_Q_log_std = self.critics[i](state, action)
+            current_Q_inv_var = torch.exp(-current_Q_log_std)
+            critic_loss += torch.mean(torch.pow(current_Q_mean - target_Q, 2) * current_Q_inv_var)
+            # var_loss = torch.mean(torch.mean(logvar, dim=-1), dim=-1)
 
         # Optimize the critic
         self.critic_optimizer.zero_grad()
@@ -207,40 +221,72 @@ class D3PG(object):
         exp version 1: constant reward func
         exp version 2: randomized reward func, not implemented yet
         '''
-        '''
         exp_target_Qs = []
         for i in range(self.exp_num_critic):
-            exp_target_Q = self.exp_critics_target[i](next_state, self.actor_target(next_state))
+            exp_target_Q, _ = self.exp_critics_target[i](next_state, self.actor_target(next_state))
             exp_target_Qs.append(exp_target_Q)
-        exp_target_Q = torch.mean(torch.cat(exp_target_Qs, dim=1), dim=1, keepdim=True)
+        if self.version == 3:
+            exp_target_Q = torch.max(torch.cat(exp_target_Qs, dim=1), dim=1, keepdim=True)[0]
+        else:
+            exp_target_Q = torch.mean(torch.cat(exp_target_Qs, dim=1), dim=1, keepdim=True)
         if self.exp_version == 0:
             exp_target_Q = reward + (not_done * self.discount * exp_target_Q).detach()
         elif self.exp_version == 1:
             exp_target_Q = constant_reward + (not_done * self.discount * exp_target_Q).detach()
-        elif self.exp_version == 2:
+        elif self.exp_version in [2, 3]:
             exp_target_Q = random_reward + (not_done * self.discount * exp_target_Q).detach()
 
         # get the critic loss
         exp_critic_loss = 0.
         for i in range(self.exp_num_critic):
-            exp_current_Q = self.exp_critics[i](state, action)
-            exp_critic_loss += F.mse_loss(exp_current_Q, exp_target_Q)
+            exp_current_Q_mean, exp_current_Q_log_std = self.exp_critics[i](state, action)
+            exp_current_Q_inv_var = torch.exp(-exp_current_Q_log_std)
+            exp_critic_loss += torch.mean(torch.pow(exp_current_Q_mean - exp_target_Q, 2) * exp_current_Q_inv_var)
 
         # Optimize the critic
         self.exp_critic_optimizer.zero_grad()
         exp_critic_loss.backward()
         self.exp_critic_optimizer.step()
-        '''
 
         '''
         Compute actor loss
         '''
-        pi_Qs_sample = []
+        pi_Qs = []
         for i in range(self.num_critic):
-            pi_Q_sample, _, _ = self.critics[i](state, self.actor(state))
-            pi_Qs_sample.append(pi_Q_sample)
-        pi_Qs_sample = torch.mean(torch.cat(pi_Qs_sample, dim=1), dim=1, keepdim=True)
-        actor_loss = -(pi_Qs_sample).mean()
+            pi_Q, _ = self.critics[i](state, self.actor(state))
+            pi_Qs.append(pi_Q)
+        pi_Qs = torch.mean(torch.cat(pi_Qs, dim=1), dim=1, keepdim=True)
+        actor_loss = -(pi_Qs).mean()
+
+        exp_actor_loss = torch.zeros([1])
+        if self.target_threshold > 0:
+            pi_action = self.actor(state).detach()
+            exp_action = (pi_action + self.exp_actor(state, pi_action)).clamp_(-1, 1)
+
+            exp_Qs = []
+            exp_Qs_stds = []
+            if self.version in [1, 3]:
+                for i in range(self.exp_num_critic):
+                    Q_mean, Q_log_std = self.exp_critics[i](state, exp_action)
+                    Q_std = torch.exp(Q_log_std)
+                    exp_Qs.append(Q_mean)
+                    exp_Qs_stds.append(Q_std)
+            elif self.version == 2:
+                for i in range(self.num_critic):
+                    Q_mean, Q_log_std = self.critics[i](state, exp_action)
+                    Q_std = torch.exp(Q_log_std)
+                    exp_Qs.append(Q_mean)
+                    exp_Qs_stds.append(Q_std)
+            else:
+                raise NotImplementedError
+
+            exp_Qs = torch.cat(exp_Qs, dim=1)
+            exp_Qs_stds = torch.cat(exp_Qs_stds, dim=1)
+            mean_exp_Qs = torch.mean(exp_Qs, dim=1)
+
+            var_exp_Q = torch.mean((torch.mean(exp_Qs ** 2, dim=1) + torch.mean(exp_Qs_stds, dim=1) - mean_exp_Qs ** 2))
+            # exp_actor_loss_2 = - std_Q.mean() / (std_Q.abs().mean().detach() + 1e-3)
+            exp_actor_loss = -var_exp_Q
 
         if self.total_it % 2 == 0:
 
@@ -248,6 +294,12 @@ class D3PG(object):
             self.actor_optimizer.zero_grad()
             actor_loss.backward()
             self.actor_optimizer.step()
+
+            if self.target_threshold > 0:
+                # Optimize the actor
+                self.exp_actor_optimizer.zero_grad()
+                exp_actor_loss.backward()
+                self.exp_actor_optimizer.step()
 
             # Update the frozen target models
             for param, target_param in zip(self.critics.parameters(), self.critics_target.parameters()):
@@ -275,7 +327,7 @@ class D3PG(object):
             self.random_reward_optimizer.step()
             random_reward_loss = random_reward_loss.item()
 
-        return actor_loss.item(), critic_loss.item(), exp_critic_loss.item(), random_reward_loss, 0, 0, 0, 0, 0
+        return actor_loss.item(), critic_loss.item(), exp_critic_loss.item(), random_reward_loss, exp_actor_loss.item(), 0, 0, 0, 0
 
 
     def save(self, filename):
---
+++ main_exp_dist.py
@@ -0,0 +1,202 @@
+import numpy as np
+import torch
+import gym
+import argparse
+import os
+import os.path as osp
+import duelingpg.utils as utils
+
+from duelingpg import d3pg_eval_ensemble_exp_aux_dist
+
+import datetime
+from torch.utils.tensorboard import SummaryWriter
+
+
+
+# Runs policy for X episodes and returns average reward
+# A fixed seed is used for the eval environment
+def eval_policy(policy, env_name, seed, eval_episodes=10):
+    eval_env = gym.make(env_name)
+    eval_env.seed(seed + 100)
+
+    avg_reward = 0.
+    for _ in range(eval_episodes):
+        state, done = eval_env.reset(), False
+        while not done:
+            action = policy.select_action(np.array(state), noisy=False)
+            state, reward, done, _ = eval_env.step(action)
+            avg_reward += reward
+
+    avg_reward /= eval_episodes
+
+    print("---------------------------------------")
+    print("Evaluation over {} episodes: {:.3f}".format(eval_episodes, avg_reward))
+    print("---------------------------------------")
+    return avg_reward
+
+
+if __name__ == "__main__":
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--policy", default="D4PG")  # Policy name (TD3, DDPG or OurDDPG, Dueling)
+    parser.add_argument("--env", default="HalfCheetah-v2")  # OpenAI gym environment name
+    parser.add_argument("--seed", default=0, type=int)  # Sets Gym, PyTorch and Numpy seeds
+    parser.add_argument("--start_timesteps", default=25e3, type=int)  # Time steps initial random policy is used
+    parser.add_argument("--eval_freq", default=5e3, type=int)  # How often (time steps) we evaluate
+    parser.add_argument("--max_timesteps", default=1e6, type=int)  # Max time steps to run environment
+    parser.add_argument("--expl_noise", default=0.1)  # Std of Gaussian exploration noise
+    parser.add_argument("--batch_size", default=256, type=int)  # Batch size for both actor and critic
+    parser.add_argument("--discount", default=0.99)  # Discount factor
+    parser.add_argument("--tau", default=0.005, type=float)  # Target network update rate
+    parser.add_argument("--policy_noise", default=0.2)  # Noise added to target policy during critic update
+    parser.add_argument("--noise_clip", default=0.5)  # Range to clip target policy noise
+    parser.add_argument("--policy_freq", default=2, type=int)  # Frequency of delayed policy updates
+    parser.add_argument("--save_model", action="store_true")  # Save model and optimizer parameters
+    parser.add_argument("--load_model", default="")  # Model load file name, "" doesn't load, "default" uses file_name
+    parser.add_argument("--version", default=3, type=int)
+    parser.add_argument("--target_threshold", default=0, type=float)
+    parser.add_argument("--num_critic", default=2, type=int)
+    parser.add_argument("--exp_version", default=1, type=int)
+    parser.add_argument("--exp_num_critic", default=2, type=int)
+
+    args = parser.parse_args()
+
+    if torch.cuda.is_available():
+        experiment_dir = '/data/zhanghc/d3pg/'
+    else:
+        experiment_dir = '/tmp/data/zhanghc/d3pg/'
+        args.start_timesteps = 2000
+    experiment_dir = experiment_dir + '12_23/'
+    writer = SummaryWriter(
+        experiment_dir + '{}_{}_{}_s{}_ver{}_thre{}_tau{}_n{}_ever{}_en{}'.format(datetime.datetime.now().strftime("%Y-%m-%d_%H-%M-%S"), args.policy, args.env, args.seed, args.version, args.target_threshold, args.tau, args.num_critic, args.exp_version, args.exp_num_critic))
+
+    file_name = "{}_{}_{}".format(args.policy, args.env, args.seed)
+    print("---------------------------------------")
+    print("Policy: {}, Env: {}, Seed: {}".format(args.policy, args.env, args.seed))
+    print("---------------------------------------")
+
+    #if not os.path.exists("./results"):
+    #    os.makedirs("./results")
+
+    if args.save_model and not os.path.exists("./models"):
+        os.makedirs("./models")
+
+    env = gym.make(args.env)
+
+    # Set seeds
+    env.seed(args.seed)
+    env.action_space.seed(args.seed)
+    torch.manual_seed(args.seed)
+    np.random.seed(args.seed)
+
+    state_dim = env.observation_space.shape[0]
+    action_dim = env.action_space.shape[0]
+    max_action = float(env.action_space.high[0])
+
+    kwargs = {
+        "state_dim": state_dim,
+        "action_dim": action_dim,
+        "max_action": max_action,
+        "discount": args.discount,
+        "tau": args.tau,
+        "num_critic": args.num_critic,
+        "exp_num_critic": args.exp_num_critic
+
+    }
+
+    # Initialize policy
+
+    kwargs['version'] = args.version
+    kwargs['exp_version'] = args.exp_version
+    kwargs['target_threshold'] = args.target_threshold
+
+    policy = d3pg_eval_ensemble_exp_aux_dist.D3PG(**kwargs)
+
+
+    replay_buffer = utils.ReplayBuffer(state_dim, action_dim)
+    onpolicy_buffer = utils.ReplayBuffer(state_dim, action_dim)
+
+    # Evaluate untrained policy
+    evaluations = [eval_policy(policy, args.env, args.seed)]
+
+    state, done = env.reset(), False
+    episode_reward = 0
+    episode_timesteps = 0
+    episode_num = 0
+
+    for t in range(int(args.max_timesteps)):
+
+        episode_timesteps += 1
+
+        # Select action randomly or according to policy
+        if t < args.start_timesteps:
+            action = env.action_space.sample()
+        else:
+            action = policy.select_action(np.array(state))
+            action = action + policy.select_noise(np.array(state), args.expl_noise)
+            action = action.clip(-max_action, max_action)
+            '''
+            action = (
+                    policy.select_action(np.array(state))
+                    + np.random.normal(0, max_action * args.expl_noise, size=action_dim)
+            ).clip(-max_action, max_action)
+            '''
+
+        # Perform action
+        next_state, reward, done, _ = env.step(action)
+        done_bool = float(done) if episode_timesteps < env._max_episode_steps else 0
+        fake_done_bool = float(done) if episode_timesteps < env._max_episode_steps else 1
+
+        # Store data in replay buffer
+        replay_buffer.add(state, action, next_state, reward, done_bool, fake_done_bool)
+
+        state = next_state
+        episode_reward += reward
+
+        # Train agent after collecting sufficient data
+        if t >= args.start_timesteps:
+            actor_loss, critic_loss, exp_critic_loss, random_reward_loss, exp_actor_loss, target_qvalue, r1,  r2, r3 = policy.train(replay_buffer, args.batch_size)
+
+            writer.add_scalar('loss/actor_loss', actor_loss, t)
+            writer.add_scalar('loss/critic_loss', critic_loss, t)
+            writer.add_scalar('loss/exp_critic_loss', exp_critic_loss, t)
+            writer.add_scalar('value/random_reward_loss', random_reward_loss, t)
+            writer.add_scalar('loss/q_value', exp_actor_loss, t)
+            writer.add_scalar('value/target_qvalue', target_qvalue, t)
+            writer.add_scalar('value/r1', r1, t)
+            writer.add_scalar('value/r2', r2, t)
+            writer.add_scalar('value/r3', r3, t)
+
+        if done:
+            writer.add_scalar('train/return', episode_reward, t)
+
+            state, done = env.reset(), False
+            episode_reward = 0
+            episode_timesteps = 0
+            episode_num += 1
+
+        # Evaluate episode
+        if (t + 1) % args.eval_freq == 0:
+            avg_return = eval_policy(policy, args.env, args.seed)
+            evaluations.append(avg_return)
+            writer.add_scalar('test/return', avg_return, t)
+            # np.save(f"./results/{file_name}", evaluations)
+
+
+        if (t + 1) % 5000000 == 0 and args.version == 0:
+            utils.test_td(args.env, policy, onpolicy_buffer)
+
+            # evaluate state, observe difference
+            value_eval, value_train, value_diff = policy.eval_value(replay_buffer)
+            writer.add_scalar('value/eval_v', value_eval, t)
+            writer.add_scalar('value/train_v', value_train, t)
+            writer.add_scalar('value/diff_v', value_diff, t)
+
+        if (t + 1) % 5000000 == 0 and args.version == 1:
+
+            utils.test_mc(args.env, policy, onpolicy_buffer)
+            # evaluate state, observe difference
+            value_eval, value_train, value_diff = policy.eval_value(replay_buffer)
+            writer.add_scalar('value/eval_v', value_eval, t)
+            writer.add_scalar('value/train_v', value_train, t)
+            writer.add_scalar('value/diff_v', value_diff, t)
