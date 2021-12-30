6f7f4ea888af0a912634dfcaa7642e297b563f80
zhanghc <zhanghc12@126.com>
2021-12-30 20:44:23 +0800

    1

---
+++ exp_runing_baseline_double.sh
@@ -0,0 +1,16 @@
+CUDA_VISIBLE_DEVICES=0 python main_exp.py --policy Dueling  --version $1  --exp_version $2 --env Walker2d-v2 --seed 4 &\
+CUDA_VISIBLE_DEVICES=1 python main_exp.py --policy Dueling  --version $1  --exp_version $2 --env Hopper-v2 --seed 4 &\
+CUDA_VISIBLE_DEVICES=0 python main_exp.py --policy Dueling --version $1  --exp_version $2 --env HalfCheetah-v2 --seed 4 &\
+CUDA_VISIBLE_DEVICES=1 python main_exp.py --policy Dueling   --version $1  --exp_version $2 --env Ant-v2 --seed 4 &\
+CUDA_VISIBLE_DEVICES=0 python main_exp.py --policy Dueling  --version $1  --exp_version $2  --env Walker2d-v2 --seed 5 &\
+CUDA_VISIBLE_DEVICES=1 python main_exp.py --policy Dueling   --version $1  --exp_version $2  --env Hopper-v2 --seed 5 &\
+CUDA_VISIBLE_DEVICES=0 python main_exp.py --policy Dueling  --version $1   --exp_version $2 --env HalfCheetah-v2 --seed 5 &\
+CUDA_VISIBLE_DEVICES=1 python main_exp.py --policy Dueling   --version $1  --exp_version $2  --env Ant-v2 --seed 5 &\
+CUDA_VISIBLE_DEVICES=0 python main_exp.py --policy Dueling   --version $1  --exp_version $2  --env Walker2d-v2 --seed 6 &\
+CUDA_VISIBLE_DEVICES=1 python main_exp.py --policy Dueling  --version $1  --exp_version $2  --env Hopper-v2 --seed 6 &\
+CUDA_VISIBLE_DEVICES=0 python main_exp.py --policy Dueling   --version $1  --exp_version $2  --env HalfCheetah-v2 --seed 6 &\
+CUDA_VISIBLE_DEVICES=1 python main_exp.py --policy Dueling  --version $1  --exp_version $2  --env Ant-v2 --seed 6 &\
+CUDA_VISIBLE_DEVICES=0 python main_exp.py --policy Dueling  --version $1  --exp_version $2  --env Walker2d-v2 --seed 7 &\
+CUDA_VISIBLE_DEVICES=1 python main_exp.py --policy Dueling  --version $1   --exp_version $2 --env Hopper-v2 --seed 7 &\
+CUDA_VISIBLE_DEVICES=0 python main_exp.py --policy Dueling  --version $1   --exp_version $2 --env HalfCheetah-v2 --seed 7 &\
+CUDA_VISIBLE_DEVICES=1 python main_exp.py --policy Dueling   --version $1 --exp_version $2  --env Ant-v2 --seed 7
