d66617d1b2eaf0c6a634ce10952d25dc0ef8c728
zhanghc <zhanghc12@126.com>
2022-01-07 20:37:58 +0800

    1

--- nn_gp.sh
+++ nn_gp.sh
@@ -1,4 +1,4 @@
-CUDAVISIBLE_DEVICES=0 python main_nngp.py --num_points 100 &\
-CUDAVISIBLE_DEVICES=1 python main_nngp.py --num_points 500 &\
-CUDAVISIBLE_DEVICES=2 python main_nngp.py --num_points 1000 &\
-CUDAVISIBLE_DEVICES=3 python main_nngp.py --num_points 2000
+CUDA_VISIBLE_DEVICES=0 python main_nngp.py --num_points 100 &\
+CUDA_VISIBLE_DEVICES=1 python main_nngp.py --num_points 500 &\
+CUDA_VISIBLE_DEVICES=2 python main_nngp.py --num_points 1000 &\
+CUDA_VISIBLE_DEVICES=3 python main_nngp.py --num_points 2000
