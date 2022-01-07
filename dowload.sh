1daa0442ac8694adfefddc7a52e34305dc5ea610
zhanghc <zhanghc12@126.com>
2022-01-07 17:26:50 +0800

    1

---
+++ dowload.sh
@@ -0,0 +1,20 @@
+wget -c 	https://rail.eecs.berkeley.edu/datasets/offline_rl/gym_mujoco_v2/hopper_expert-v2.hdf5 &\
+wget -c 	https://rail.eecs.berkeley.edu/datasets/offline_rl/gym_mujoco_v2/hopper_full_replay-v2.hdf5 &\
+wget -c 	https://rail.eecs.berkeley.edu/datasets/offline_rl/gym_mujoco_v2/hopper_medium.hdf5 &\
+wget -c 	https://rail.eecs.berkeley.edu/datasets/offline_rl/gym_mujoco_v2/hopper_medium_expert-v2.hdf5 &\
+wget -c 	https://rail.eecs.berkeley.edu/datasets/offline_rl/gym_mujoco_v2/hopper_medium_replay-v2.hdf5 &\
+wget -c 	https://rail.eecs.berkeley.edu/datasets/offline_rl/gym_mujoco_v2/hopper_random-v2.hdf5 &\
+wget -c 	https://rail.eecs.berkeley.edu/datasets/offline_rl/gym_mujoco_v2/waler2d_expert-v2.hdf5 &\
+wget -c 	https://rail.eecs.berkeley.edu/datasets/offline_rl/gym_mujoco_v2/waler2d_full_replay-v2.hdf5 &\
+wget -c 	https://rail.eecs.berkeley.edu/datasets/offline_rl/gym_mujoco_v2/waler2d_medium.hdf5 &\
+wget -c 	https://rail.eecs.berkeley.edu/datasets/offline_rl/gym_mujoco_v2/waler2d_medium_expert-v2.hdf5 &\
+wget -c 	https://rail.eecs.berkeley.edu/datasets/offline_rl/gym_mujoco_v2/waler2d_medium_replay-v2.hdf5 &\
+wget -c 	https://rail.eecs.berkeley.edu/datasets/offline_rl/gym_mujoco_v2/waler2d_random-v2.hdf5 &\
+wget -c 	https://rail.eecs.berkeley.edu/datasets/offline_rl/gym_mujoco_v2/halfcheetah_expert-v2.hdf5 &\
+wget -c 	https://rail.eecs.berkeley.edu/datasets/offline_rl/gym_mujoco_v2/halfcheetah_full_replay-v2.hdf5 &\
+wget -c 	https://rail.eecs.berkeley.edu/datasets/offline_rl/gym_mujoco_v2/halfcheetah_medium.hdf5 &\
+wget -c 	https://rail.eecs.berkeley.edu/datasets/offline_rl/gym_mujoco_v2/halfcheetah_medium_expert-v2.hdf5 &\
+wget -c 	https://rail.eecs.berkeley.edu/datasets/offline_rl/gym_mujoco_v2/halfcheetah_medium_replay-v2.hdf5 &\
+wget -c 	https://rail.eecs.berkeley.edu/datasets/offline_rl/gym_mujoco_v2/halfcheetah_random-v2.hdf5
+
+
