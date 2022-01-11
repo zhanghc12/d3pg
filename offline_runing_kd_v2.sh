69e375174a9232db7d14b6f9f4d0a9138b1cc89f
zhanghc <zhanghc12@126.com>
2022-01-11 21:17:08 +0800

    1

--- offline_runing_kd_v2.sh
+++ offline_runing_kd_v2.sh
@@ -1,11 +1,11 @@
-CUDA_VISIBLE_DEVICES=0 python offline_kd.py --env hopper-expert-v2  --bc_scale 0 --seed 1 --loading 1 --k 10 &\
-CUDA_VISIBLE_DEVICES=1 python offline_kd.py --env hopper-random-v2  --bc_scale 0 --seed 1 --loading 0 --k 10 &\
-CUDA_VISIBLE_DEVICES=2 python offline_kd.py --env hopper-medium-v2  --bc_scale 0 --seed 1 --loading 0 --k 10 &\
-CUDA_VISIBLE_DEVICES=3 python offline_kd.py --env hopper-medium-exert-v2  --bc_scale 0 --seed 1 --loading 0 --k 10 &\
-CUDA_VISIBLE_DEVICES=0 python offline_kd.py --env hopper-medium-replay-v2  --bc_scale 0 --seed 1 --loading 0 --k 10 &\
-CUDA_VISIBLE_DEVICES=1 python offline_kd.py --env walker2d-expert-v2  --bc_scale 0 --seed 1 --loading 0 --k 10 &\
-CUDA_VISIBLE_DEVICES=2 python offline_kd.py --env walker2d-random-v2  --bc_scale 0 --seed 1 --loading 0 --k 10 &\
-CUDA_VISIBLE_DEVICES=3 python offline_kd.py --env walker2d-medium-v2  --bc_scale 0 --seed 1 --loading 0 --k 10 &\
+CUDA_VISIBLE_DEVICES=0 python offline_kd.py --env hopper-expert-v2  --bc_scale 0 --seed 1 --loading 0 --k 2 &\
+CUDA_VISIBLE_DEVICES=1 python offline_kd.py --env hopper-random-v2  --bc_scale 0 --seed 1 --loading 0 --k 2 &\
+CUDA_VISIBLE_DEVICES=2 python offline_kd.py --env hopper-medium-v2  --bc_scale 0 --seed 1 --loading 0 --k 2 &\
+CUDA_VISIBLE_DEVICES=3 python offline_kd.py --env hopper-medium-exert-v2  --bc_scale 0 --seed 1 --loading 0 --k 2 &\
+CUDA_VISIBLE_DEVICES=0 python offline_kd.py --env hopper-medium-replay-v2  --bc_scale 0 --seed 1 --loading 0 --k 2 &\
+CUDA_VISIBLE_DEVICES=1 python offline_kd.py --env walker2d-expert-v2  --bc_scale 0 --seed 1 --loading 0 --k 2 &\
+CUDA_VISIBLE_DEVICES=2 python offline_kd.py --env walker2d-random-v2  --bc_scale 0 --seed 1 --loading 0 --k 2 &\
+CUDA_VISIBLE_DEVICES=3 python offline_kd.py --env walker2d-medium-v2  --bc_scale 0 --seed 1 --loading 0 --k 2 &\
 
 # sh offline_rl_large_scale_single_seed_new.sh 31  1.0 -1 0.2  0.05
 
