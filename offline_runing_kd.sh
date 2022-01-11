4f117cb5a6f26265eff6ca303e5bddf1756cd365
zhanghc <zhanghc12@126.com>
2022-01-11 16:24:32 +0800

    1

---
+++ offline_runing_kd.sh
@@ -0,0 +1,21 @@
+# CUDA_VISIBLE_DEVICES=0 python offline_sf.py --env hopper-expert-v2 --seed 0 &\
+CUDA_VISIBLE_DEVICES=1 python offline_kd.py --env hopper-expert-v2  --bc_sacle 0.1 --seed 1 --loading 1 --k 10 &\
+CUDA_VISIBLE_DEVICES=2 python offline_kd.py --env hopper-expert-v2  --bc_sacle 10 --seed 0 --loading 1 --k 10 &\
+CUDA_VISIBLE_DEVICES=3 python offline_kd.py --env hopper-expert-v2  --bc_sacle 100 --seed 1 --loading 1 --k 10
+
+# sh offline_rl_large_scale_single_seed_new.sh 31  1.0 -1 0.2  0.05
+
+
+
+# CUDA_VISIBLE_DEVICES=0 python offline_tqc.py --env hopper-random-v2
+
+# pip3 install torch==1.10.0+cu110  -f https://download.pytorch.org/whl/cu113/torch_stable.html
+
+# conda install pytorch  cudatoolkit=11.0 -c pytorch-lts -c nvidia
+
+# conda install pytorch  cudatoolkit=11.3 -c pytorch
+
+# conda install pytorch  cudatoolkit=11.3 -c pytorch
+
+# pip3 install torch==1.10.0+cu113 -f https://download.pytorch.org/whl/cu113/torch_stable.html
+
