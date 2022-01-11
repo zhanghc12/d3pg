a4ad3a16652010d722f650301f8410b9804ff5c9
zhanghc <zhanghc12@126.com>
2022-01-11 09:25:50 +0800

    1

--- sf_offline/td3_v3.py
+++ sf_offline/td3_v3.py
@@ -115,12 +115,15 @@ class TD3(object):
             state_batch, action_batch = memory.sample_by_index(ind=index)
             test_action_batch = 1 * torch.normal(torch.zeros_like(action_batch), torch.ones_like(action_batch))
             test_state_batch = 1 * torch.normal(torch.zeros_like(state_batch), torch.ones_like(state_batch))
+            test_action_batch2 = action_batch + 1 * torch.normal(torch.zeros_like(action_batch), torch.ones_like(action_batch))
 
             test_action_batch = torch.clamp_(test_action_batch, -1, 1)
             psi = self.bc_critic.get_psi(state_batch, action_batch)
             psi_norm = psi.norm(dim=1, p=1)
 
-            test_psi = self.bc_critic.get_psi(test_state_batch, test_action_batch)
+            # test_psi = self.bc_critic.get_psi(test_state_batch, test_action_batch)
+            test_psi = self.bc_critic.get_psi(state_batch, test_action_batch2)
+
             test_psi_norm = test_psi.norm(dim=1, p=1)
             i += batch_size
             self.psi_list.extend(psi_norm.detach().cpu().numpy())
