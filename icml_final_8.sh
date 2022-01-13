aee723222d245121b0943a23ad1f4923ee76f716
zhanghc <zhanghc12@126.com>
2022-01-13 20:03:09 +0800

    1

--- icml_final_8.sh
+++ icml_final_8.sh
@@ -15,7 +15,10 @@ CUDA_VISIBLE_DEVICES=5 python offline_kd_final.py --policy TQC  --version $1  --
 CUDA_VISIBLE_DEVICES=6 python offline_kd_final.py --policy TQC  --version $1  --bc_scale $2  --env walker2d-expert-v2 --seed $3 --top_quantiles_to_drop $4
 
 
+# CUDA_VISIBLE_DEVICES=6 python offline_kd_final.py --policy TQC  --version 3  --bc_scale 20  --env walker2d-expert-v2 --seed 0 --top_quantiles_to_drop 0
+
+
 # sh offline_rl_large_scale_single_seed_new.sh 31  1.0 -1 0.2  0.05
 
 
-
+# sh icml_final.sh 1 20 0 0
