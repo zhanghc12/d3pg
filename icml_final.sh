835f0915687426a36d2c8784dfb63d165f4198c1
zhanghc <zhanghc12@126.com>
2022-01-14 10:52:30 +0800

    1

--- icml_final.sh
+++ icml_final.sh
@@ -1,18 +1,18 @@
-CUDA_VISIBLE_DEVICES=0 python offline_kd_final.py --policy TQC  --version $1  --bc_scale $2 --env hopper-random-v2 --seed $3 &\
-CUDA_VISIBLE_DEVICES=1 python offline_kd_final.py --policy TQC  --version $1  --bc_scale $2  --env hopper-medium-v2  --seed $3 &\
-CUDA_VISIBLE_DEVICES=2 python offline_kd_final.py --policy TQC  --version $1  --bc_scale $2  --env hopper-medium-expert-v2  --seed $3 &\
-CUDA_VISIBLE_DEVICES=3 python offline_kd_final.py --policy TQC  --version $1  --bc_scale $2  --env hopper-medium-replay-v2 --seed $3 &\
-CUDA_VISIBLE_DEVICES=0 python offline_kd_final.py --policy TQC  --version $1  --bc_scale $2  --env hopper-expert-v2 --seed $3 &\
-CUDA_VISIBLE_DEVICES=1 python offline_kd_final.py --policy TQC  --version $1  --bc_scale $2 --env halfcheetah-random-v2 --seed $3 &\
-CUDA_VISIBLE_DEVICES=2 python offline_kd_final.py --policy TQC  --version $1  --bc_scale $2  --env halfcheetah-medium-v2  --seed $3 &\
-CUDA_VISIBLE_DEVICES=3 python offline_kd_final.py --policy TQC  --version $1  --bc_scale $2  --env halfcheetah-medium-expert-v2  --seed $3 &\
-CUDA_VISIBLE_DEVICES=0 python offline_kd_final.py --policy TQC  --version $1  --bc_scale $2  --env halfcheetah-medium-replay-v2 --seed $3 &\
-CUDA_VISIBLE_DEVICES=1 python offline_kd_final.py --policy TQC  --version $1  --bc_scale $2  --env halfcheetah-expert-v2 --seed $3 &\
-CUDA_VISIBLE_DEVICES=2 python offline_kd_final.py --policy TQC  --version $1  --bc_scale $2 --env walker2d-random-v2 --seed $3 &\
-CUDA_VISIBLE_DEVICES=3 python offline_kd_final.py --policy TQC  --version $1  --bc_scale $2  --env walker2d-medium-v2  --seed $3 &\
-CUDA_VISIBLE_DEVICES=0 python offline_kd_final.py --policy TQC  --version $1  --bc_scale $2  --env walker2d-medium-expert-v2  --seed $3 &\
-CUDA_VISIBLE_DEVICES=1 python offline_kd_final.py --policy TQC  --version $1  --bc_scale $2  --env walker2d-medium-replay-v2 --seed $3 &\
-CUDA_VISIBLE_DEVICES=2 python offline_kd_final.py --policy TQC  --version $1  --bc_scale $2  --env walker2d-expert-v2 --seed $3
+CUDA_VISIBLE_DEVICES=0 python offline_kd_final.py --policy TQC  --version $1  --bc_scale $2 --env hopper-random-v0 --seed $3 &\
+CUDA_VISIBLE_DEVICES=1 python offline_kd_final.py --policy TQC  --version $1  --bc_scale $2  --env hopper-medium-v0  --seed $3 &\
+CUDA_VISIBLE_DEVICES=2 python offline_kd_final.py --policy TQC  --version $1  --bc_scale $2  --env hopper-medium-expert-v0  --seed $3 &\
+CUDA_VISIBLE_DEVICES=3 python offline_kd_final.py --policy TQC  --version $1  --bc_scale $2  --env hopper-medium-replay-v0 --seed $3 &\
+CUDA_VISIBLE_DEVICES=0 python offline_kd_final.py --policy TQC  --version $1  --bc_scale $2  --env hopper-expert-v0 --seed $3 &\
+CUDA_VISIBLE_DEVICES=1 python offline_kd_final.py --policy TQC  --version $1  --bc_scale $2 --env halfcheetah-random-v0 --seed $3 &\
+CUDA_VISIBLE_DEVICES=2 python offline_kd_final.py --policy TQC  --version $1  --bc_scale $2  --env halfcheetah-medium-v0  --seed $3 &\
+CUDA_VISIBLE_DEVICES=3 python offline_kd_final.py --policy TQC  --version $1  --bc_scale $2  --env halfcheetah-medium-expert-v0  --seed $3 &\
+CUDA_VISIBLE_DEVICES=0 python offline_kd_final.py --policy TQC  --version $1  --bc_scale $2  --env halfcheetah-medium-replay-v0 --seed $3 &\
+CUDA_VISIBLE_DEVICES=1 python offline_kd_final.py --policy TQC  --version $1  --bc_scale $2  --env halfcheetah-expert-v0 --seed $3 &\
+CUDA_VISIBLE_DEVICES=2 python offline_kd_final.py --policy TQC  --version $1  --bc_scale $2 --env walker2d-random-v0 --seed $3 &\
+CUDA_VISIBLE_DEVICES=3 python offline_kd_final.py --policy TQC  --version $1  --bc_scale $2  --env walker2d-medium-v0  --seed $3 &\
+CUDA_VISIBLE_DEVICES=0 python offline_kd_final.py --policy TQC  --version $1  --bc_scale $2  --env walker2d-medium-expert-v0  --seed $3 &\
+CUDA_VISIBLE_DEVICES=1 python offline_kd_final.py --policy TQC  --version $1  --bc_scale $2  --env walker2d-medium-replay-v0 --seed $3 &\
+CUDA_VISIBLE_DEVICES=2 python offline_kd_final.py --policy TQC  --version $1  --bc_scale $2  --env walker2d-expert-v0 --seed $3
 
 
 # sh offline_rl_large_scale_single_seed_new.sh 31  1.0 -1 0.2  0.05
