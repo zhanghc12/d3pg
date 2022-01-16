02423ee780e924bf022bea0d5a1ead881ceeda73
zhanghc <zhanghc12@126.com>
2022-01-16 10:38:08 +0800

    1

--- offline_kd_test_v3.py
+++ offline_kd_test_v3.py
@@ -35,6 +35,8 @@ def load_hdf5(dataset, replay_buffer):
 
     print(np.std(dataset['actions'], axis=0, keepdims=True))
 
+    replay_buffer.action = normalize(dataset['actions'])
+
     replay_buffer.next_state = normalize(dataset['next_observations'])
     replay_buffer.reward = np.expand_dims(np.squeeze(dataset['rewards']), 1)
     replay_buffer.not_done = 1 - np.expand_dims(np.squeeze(dataset['terminals']), 1)
