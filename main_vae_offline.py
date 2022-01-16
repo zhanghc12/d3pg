20f3496eacf001593e744378334af9b4172c3934
zhanghc <zhanghc12@126.com>
2022-01-16 10:22:46 +0800

    1

--- main_vae_offline.py
+++ main_vae_offline.py
@@ -9,6 +9,8 @@ import duelingpg.utils as utils
 import d4rl
 import d4rl.gym_mujoco
 from mc_dropout.drop import FlattenDropout_Mlp, VAEPolicy
+import torch.nn as nn
+import torch.nn.functional as F
 
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
@@ -125,6 +127,62 @@ def test_uncertainty(memory, vae_policy, batch_size=2560):
     return iid_list
 
 
+class VAEPolicy(nn.Module):
+    def __init__(
+            self,
+            obs_dim,
+            action_dim,
+            latent_dim,
+    ):
+        super().__init__(
+        )
+        self.latent_dim = latent_dim
+
+        self.e1 = torch.nn.Linear(obs_dim + action_dim, 750)
+        self.e2 = torch.nn.Linear(750, 750)
+
+        self.mean = torch.nn.Linear(750, self.latent_dim)
+        self.log_std = torch.nn.Linear(750, self.latent_dim)
+
+        self.d1 = torch.nn.Linear(obs_dim + self.latent_dim, 750)
+        self.d2 = torch.nn.Linear(750, 750)
+        self.d3 = torch.nn.Linear(750, action_dim)
+
+        self.max_action = 1.0
+        self.latent_dim = latent_dim
+
+    def forward(self, state, action):
+        z = F.relu(self.e1(torch.cat([state, action], 1)))
+        z = F.relu(self.e2(z))
+
+        mean = self.mean(z)
+        # Clamped for numerical stability
+        log_std = self.log_std(z).clamp(-4, 15)
+        std = torch.exp(log_std)
+        z = mean + std * torch.FloatTensor(np.random.normal(0, 1, size=(std.size()))).to(device)
+
+        u = self.decode(state, z)
+
+        return u, mean, std
+
+    def decode(self, state, z=None):
+        if z is None:
+            z = torch.FloatTensor(np.random.normal(0, 1, size=(state.size(0), self.latent_dim))).clamp(-0.5, 0.5).to(device)
+
+        a = F.relu(self.d1(torch.cat([state, z], 1)))
+        a = F.relu(self.d2(a))
+        return torch.tanh(self.d3(a))
+
+    def decode_multiple(self, state, z=None, num_decode=10, device='cpu'):
+        if z is None:
+            z = torch.FloatTensor(np.random.normal(0, 1, size=(state.size(0), num_decode, self.latent_dim))).clamp(-0.5,
+                                                                                                                0.5).to(device)
+
+        a = F.relu(self.d1(torch.cat([state.unsqueeze(0).repeat(num_decode, 1, 1).permute(1, 0, 2), z], 2)))
+        a = F.relu(self.d2(a))
+        return torch.tanh(self.d3(a)), self.d3(a)
+
+
 def main(args=None):
     if args is None:
         args = readParser()
@@ -165,12 +223,15 @@ def main(args=None):
         'spectral_norm': True
     }
 
+    vae_policy = VAEPolicy(state_dim, action_dim, action_dim * 2).to(device)
+    '''
     vae_policy = VAEPolicy(
         obs_dim=obs_dim,
         action_dim=action_dim,
         hidden_sizes=[750, 750],
         latent_dim=action_dim * 2,
     ).to(device)
+    '''
 
     vae_dirname = dirname + args.env_name + '/model_' + str(args.version)
 
