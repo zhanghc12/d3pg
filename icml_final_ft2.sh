f565240358b92b8a6aec8fc22b3d6d8cf4b7bcef
zhanghc <zhanghc12@126.com>
2022-01-20 19:44:50 +0800

    1

--- icml_final_ft2.sh
+++ icml_final_ft2.sh
@@ -1,13 +1,13 @@
 CUDA_VISIBLE_DEVICES=0 python offline_kd_final.py --policy TQC  --version $1  --bc_scale 50  --env halfcheetah-expert-v2 --seed $3 --top_quantiles_to_drop $4  &\
 CUDA_VISIBLE_DEVICES=1 python offline_kd_final.py --policy TQC  --version $1  --bc_scale 100  --env halfcheetah-expert-v2 --seed $3 --top_quantiles_to_drop $4  &\
 CUDA_VISIBLE_DEVICES=2 python offline_kd_final.py --policy TQC  --version $1  --bc_scale 15  --env walker2d-medium-v2  --seed $3 --top_quantiles_to_drop $4  &\
-CUDA_VISIBLE_DEVICES=3 python offline_kd_final.py --policy TQC  --version $1  --bc_scale 3  --env walker2d-medium-expert-v2  --seed $3 --top_quantiles_to_drop $4  &\
-CUDA_VISIBLE_DEVICES=0 python offline_kd_final.py --policy TQC  --version $1  --bc_scale 4  --env walker2d-medium-expert-v2  --seed $3 --top_quantiles_to_drop $4  &\
-CUDA_VISIBLE_DEVICES=1 python offline_kd_final.py --policy TQC  --version $1  --bc_scale 3  --env walker2d-expert-v2 --seed $3 --top_quantiles_to_drop $4 &\
-CUDA_VISIBLE_DEVICES=2 python offline_kd_final.py --policy TQC  --version $1  --bc_scale 4  --env walker2d-expert-v2 --seed $3 --top_quantiles_to_drop $4 &\
-CUDA_VISIBLE_DEVICES=3 python offline_kd_final.py --policy TQC  --version $1  --bc_scale 1 --env halfcheetah-random-v2 --seed $3 --top_quantiles_to_drop $4  &\
-CUDA_VISIBLE_DEVICES=0 python offline_kd_final.py --policy TQC  --version $1  --bc_scale 0.5 --env halfcheetah-random-v2 --seed $3 --top_quantiles_to_drop $4  &\
-CUDA_VISIBLE_DEVICES=1 python offline_kd_final.py --policy TQC  --version $1  --bc_scale 1.5 --env halfcheetah-random-v2 --seed $3 --top_quantiles_to_drop $4
+CUDA_VISIBLE_DEVICES=0 python offline_kd_final.py --policy TQC  --version $1  --bc_scale 3  --env walker2d-medium-expert-v2  --seed $3 --top_quantiles_to_drop $4  &\
+CUDA_VISIBLE_DEVICES=1 python offline_kd_final.py --policy TQC  --version $1  --bc_scale 4  --env walker2d-medium-expert-v2  --seed $3 --top_quantiles_to_drop $4  &\
+CUDA_VISIBLE_DEVICES=2 python offline_kd_final.py --policy TQC  --version $1  --bc_scale 3  --env walker2d-expert-v2 --seed $3 --top_quantiles_to_drop $4 &\
+CUDA_VISIBLE_DEVICES=0 python offline_kd_final.py --policy TQC  --version $1  --bc_scale 4  --env walker2d-expert-v2 --seed $3 --top_quantiles_to_drop $4 &\
+CUDA_VISIBLE_DEVICES=1 python offline_kd_final.py --policy TQC  --version $1  --bc_scale 1 --env halfcheetah-random-v2 --seed $3 --top_quantiles_to_drop $4  &\
+CUDA_VISIBLE_DEVICES=2 python offline_kd_final.py --policy TQC  --version $1  --bc_scale 0.5 --env halfcheetah-random-v2 --seed $3 --top_quantiles_to_drop $4  &\
+CUDA_VISIBLE_DEVICES=0 python offline_kd_final.py --policy TQC  --version $1  --bc_scale 1.5 --env halfcheetah-random-v2 --seed $3 --top_quantiles_to_drop $4
 
 
 # CUDA_VISIBLE_DEVICES=6 python offline_kd_final.py --policy TQC  --version 3  --bc_scale 20  --env walker2d-expert-v2 --seed 0 --top_quantiles_to_drop 0
