f1c3bff6e03fe79a164e499430ec2d973112d049
zhanghc <zhanghc12@126.com>
2022-04-21 16:06:50 +0800

    1

--- eval_ac_dnpg_4gpu.sh
+++ eval_ac_dnpg_4gpu.sh
@@ -9,11 +9,22 @@
 #CUDA_VISIBLE_DEVICES=2 python main_dnpg.py --policy D4PG  --version 0  --target_threshold 1.0   --env $1 --test 0 &\
 #CUDA_VISIBLE_DEVICES=3 python main_dnpg.py --policy D4PG  --version 0  --target_threshold 0.0   --env $1 --test 0
 
-CUDA_VISIBLE_DEVICES=0 python main_dnpg_v3.py --policy D4PG  --version $2  --target_threshold 0.0001  --env $1  &\
-CUDA_VISIBLE_DEVICES=1 python main_dnpg_v3.py --policy D4PG  --version $2  --target_threshold 0.001   --env $1  &\
-CUDA_VISIBLE_DEVICES=2 python main_dnpg_v3.py --policy D4PG  --version $2  --target_threshold 0.01   --env $1   &\
-CUDA_VISIBLE_DEVICES=3 python main_dnpg_v3.py --policy D4PG  --version $2  --target_threshold 0.1   --env $1
-
+CUDA_VISIBLE_DEVICES=0 python main_dnpg_v3.py --policy D4PG  --version $2  --target_threshold 0.0001  --env Walker2d-v2  &\
+CUDA_VISIBLE_DEVICES=1 python main_dnpg_v3.py --policy D4PG  --version $2  --target_threshold 0.001   --env Walker2d-v2  &\
+CUDA_VISIBLE_DEVICES=2 python main_dnpg_v3.py --policy D4PG  --version $2  --target_threshold 0.01   --env Walker2d-v2   &\
+CUDA_VISIBLE_DEVICES=3 python main_dnpg_v3.py --policy D4PG  --version $2  --target_threshold 0.1   --env Walker2d-v2 &\
+CUDA_VISIBLE_DEVICES=0 python main_dnpg_v3.py --policy D4PG  --version $2  --target_threshold 0.0001  --env Hopper-v2  &\
+CUDA_VISIBLE_DEVICES=1 python main_dnpg_v3.py --policy D4PG  --version $2  --target_threshold 0.001   --env Hopper-v2  &\
+CUDA_VISIBLE_DEVICES=2 python main_dnpg_v3.py --policy D4PG  --version $2  --target_threshold 0.01   --env Hopper-v2   &\
+CUDA_VISIBLE_DEVICES=3 python main_dnpg_v3.py --policy D4PG  --version $2  --target_threshold 0.1   --env Hopper-v2 &\
+CUDA_VISIBLE_DEVICES=0 python main_dnpg_v3.py --policy D4PG  --version $2  --target_threshold 0.0001  --env HalfCheetah-v2  &\
+CUDA_VISIBLE_DEVICES=1 python main_dnpg_v3.py --policy D4PG  --version $2  --target_threshold 0.001   --env HalfCheetah-v2  &\
+CUDA_VISIBLE_DEVICES=2 python main_dnpg_v3.py --policy D4PG  --version $2  --target_threshold 0.01   --env HalfCheetah-v2   &\
+CUDA_VISIBLE_DEVICES=3 python main_dnpg_v3.py --policy D4PG  --version $2  --target_threshold 0.1   --env HalfCheetah-v2 &\
+CUDA_VISIBLE_DEVICES=0 python main_dnpg_v3.py --policy D4PG  --version $2  --target_threshold 0.0001  --env Ant-v2  &\
+CUDA_VISIBLE_DEVICES=1 python main_dnpg_v3.py --policy D4PG  --version $2  --target_threshold 0.001   --env Ant-v2  &\
+CUDA_VISIBLE_DEVICES=2 python main_dnpg_v3.py --policy D4PG  --version $2  --target_threshold 0.01   --env Ant-v2   &\
+CUDA_VISIBLE_DEVICES=3 python main_dnpg_v3.py --policy D4PG  --version $2  --target_threshold 0.1   --env Ant-v2
 
 #sh eval_ac_dnpg_4gpu.sh Walker2d-v2 3
 #sh eval_ac_dnpg_4gpu.sh Hopper-v2 3
