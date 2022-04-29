4d86e8c16eb60e8d12675b42dbab00aecffb6f74
zhanghc <zhanghc12@126.com>
2022-04-29 11:12:54 +0800

    1

--- eval_dnpg_4gpu_model.sh
+++ eval_dnpg_4gpu_model.sh
@@ -1,5 +1,5 @@
 CUDA_VISIBLE_DEVICES=0 python main_dnpg_v4.py --policy D4PG  --version $1 --ratio $2  --target_threshold $3 --seed 11 --env HalfCheetah-v2  &\
 CUDA_VISIBLE_DEVICES=1 python main_dnpg_v4.py --policy D4PG  --version $1 --ratio $2  --target_threshold $3 --seed 12  --env HalfCheetah-v2  &\
 CUDA_VISIBLE_DEVICES=2 python main_dnpg_v4.py --policy D4PG  --version $1 --ratio $2  --target_threshold $3 --seed 13  --env HalfCheetah-v2   &\
-CUDA_VISIBLE_DEVICES=3 python main_dnpg_v4.py --policy D4PG  --version $1 --ratio $2  --target_threshold $3 --seed 14  --env HalfCheetah-v2 &\
+CUDA_VISIBLE_DEVICES=3 python main_dnpg_v4.py --policy D4PG  --version $1 --ratio $2  --target_threshold $3 --seed 14  --env HalfCheetah-v2
 
