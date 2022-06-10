8e9f0b0b64e78db5cbaab0553b3f85c4178eaba4
zhanghc <zhanghc12@126.com>
2022-06-10 10:48:55 +0800

    1

--- duelingpg/utils.py
+++ duelingpg/utils.py
@@ -1,6 +1,200 @@
 import numpy as np
 import torch
 import gym
+from knn_ue.sum_tree import SumSegmentTree, MinSegmentTree
+import random
+
+
+class OriginalReplayBuffer(object):
+    def __init__(self, size):
+        """
+        Implements a ring buffer (FIFO).
+        :param size: (int)  Max number of transitions to store in the buffer. When the buffer overflows the old
+            memories are dropped.
+        """
+        self._storage = []
+        self._maxsize = size
+        self._next_idx = 0
+        self.device = torch.device(
+            "cuda" if torch.cuda.is_available() else "cpu")
+
+    def __len__(self):
+        return len(self._storage)
+
+    @property
+    def storage(self):
+        """[(Union[np.ndarray, int], Union[np.ndarray, int], float, Union[np.ndarray, int], bool)]: content of the replay buffer"""
+        return self._storage
+
+    @property
+    def buffer_size(self):
+        """float: Max capacity of the buffer"""
+        return self._maxsize
+
+    def can_sample(self, n_samples):
+        """
+        Check if n_samples samples can be sampled
+        from the buffer.
+        :param n_samples: (int)
+        :return: (bool)
+        """
+        return len(self) >= n_samples
+
+    def is_full(self):
+        """
+        Check whether the replay buffer is full or not.
+        :return: (bool)
+        """
+        return len(self) == self.buffer_size
+
+    def add(self, obs_t, action, reward, obs_tp1, done, action_tp1):
+        """
+        add a new transition to the buffer
+        :param obs_t: (Union[np.ndarray, int]) the last observation
+        :param action: (Union[np.ndarray, int]) the action
+        :param reward: (float) the reward of the transition
+        :param obs_tp1: (Union[np.ndarray, int]) the current observation
+        :param done: (bool) is the episode done
+        """
+        data = (obs_t, action, reward, obs_tp1, done, action_tp1)
+
+        if self._next_idx >= len(self._storage):
+            self._storage.append(data)
+        else:
+            self._storage[self._next_idx] = data
+        self._next_idx = (self._next_idx + 1) % self._maxsize
+
+
+    def _encode_sample(self, idxes):
+        obses_t, actions, rewards, obses_tp1, dones, actions_tp1 = [], [], [], [], [], []
+        for i in idxes:
+            data = self._storage[i]
+            obs_t, action, reward, obs_tp1, done, action_tp1 = data
+            obses_t.append(np.array(obs_t, copy=False))
+            actions.append(np.array(action, copy=False))
+            rewards.append(np.array(reward, copy=False))
+            obses_tp1.append(np.array(obs_tp1, copy=False))
+            dones.append(np.array(done, copy=False))
+            actions_tp1.append(np.array(action_tp1, copy=False))
+
+        return (torch.FloatTensor(np.array(obses_t)).to(self.device),
+                torch.FloatTensor(np.array(actions)).to(self.device),
+                torch.FloatTensor(np.array(rewards)).to(self.device),
+                torch.FloatTensor(np.array(obses_tp1)).to(self.device),
+                torch.FloatTensor(np.array(dones)).to(self.device),
+                torch.FloatTensor(np.array(actions_tp1)).to(self.device)
+                )
+
+    def sample(self, batch_size):
+        """
+        Sample a batch of experiences.
+        :param batch_size: (int) How many transitions to sample.
+        :param env: (Optional[VecNormalize]) associated gym VecEnv
+            to normalize the observations/rewards when sampling
+        :return:
+            - obs_batch: (np.ndarray) batch of observations
+            - act_batch: (numpy float) batch of actions executed given obs_batch
+            - rew_batch: (numpy float) rewards received as results of executing act_batch
+            - next_obs_batch: (np.ndarray) next set of observations seen after executing act_batch
+            - done_mask: (numpy bool) done_mask[i] = 1 if executing act_batch[i] resulted in the end of an episode
+                and 0 otherwise.
+        """
+        idxes = [random.randint(0, len(self._storage) - 1) for _ in range(batch_size)]
+        return self._encode_sample(idxes)
+
+
+class PrioritizedReplayBuffer(OriginalReplayBuffer):
+    def __init__(self, size, alpha):
+        """
+        Create Prioritized Replay buffer.
+        See Also ReplayBuffer.__init__
+        :param size: (int) Max number of transitions to store in the buffer. When the buffer overflows the old memories
+            are dropped.
+        :param alpha: (float) how much prioritization is used (0 - no prioritization, 1 - full prioritization)
+        """
+        super(PrioritizedReplayBuffer, self).__init__(size)
+        assert alpha >= 0
+        self._alpha = alpha
+
+        it_capacity = 1
+        while it_capacity < size:
+            it_capacity *= 2
+
+        self._it_sum = SumSegmentTree(it_capacity)
+        self._it_min = MinSegmentTree(it_capacity)
+        self._max_priority = 1.0
+
+    def add(self, obs_t, action, reward, obs_tp1, done, action_tp1):
+        """
+        add a new transition to the buffer
+        :param obs_t: (Any) the last observation
+        :param action: ([float]) the action
+        :param reward: (float) the reward of the transition
+        :param obs_tp1: (Any) the current observation
+        :param done: (bool) is the episode done
+        """
+        idx = self._next_idx
+        super().add(obs_t, action, reward, obs_tp1, done, action_tp1)
+        self._it_sum[idx] = self._max_priority ** self._alpha
+        self._it_min[idx] = self._max_priority ** self._alpha
+
+    def _sample_proportional(self, batch_size):
+        mass = []
+        total = self._it_sum.sum(0, len(self._storage) - 1)
+        # TODO(szymon): should we ensure no repeats?
+        mass = np.random.random(size=batch_size) * total
+        idx = self._it_sum.find_prefixsum_idx(mass)
+        return idx
+
+    def sample(self, batch_size, beta=0):
+        """
+        Sample a batch of experiences.
+        compared to ReplayBuffer.sample
+        it also returns importance weights and idxes
+        of sampled experiences.
+        :param batch_size: (int) How many transitions to sample.
+        :param beta: (float) To what degree to use importance weights (0 - no corrections, 1 - full correction)
+        :param env: (Optional[VecNormalize]) associated gym VecEnv
+            to normalize the observations/rewards when sampling
+        :return:
+            - obs_batch: (np.ndarray) batch of observations
+            - act_batch: (numpy float) batch of actions executed given obs_batch
+            - rew_batch: (numpy float) rewards received as results of executing act_batch
+            - next_obs_batch: (np.ndarray) next set of observations seen after executing act_batch
+            - done_mask: (numpy bool) done_mask[i] = 1 if executing act_batch[i] resulted in the end of an episode
+                and 0 otherwise.
+            - weights: (numpy float) Array of shape (batch_size,) and dtype np.float32 denoting importance weight of
+                each sampled transition
+            - idxes: (numpy int) Array of shape (batch_size,) and dtype np.int32 idexes in buffer of sampled experiences
+        """
+        assert beta > 0
+
+        idxes = self._sample_proportional(batch_size)
+        weights = []
+        p_min = self._it_min.min() / self._it_sum.sum()
+        max_weight = (p_min * len(self._storage)) ** (-beta)
+        p_sample = self._it_sum[idxes] / self._it_sum.sum()
+        weights = (p_sample * len(self._storage)) ** (-beta) / max_weight
+        encoded_sample = self._encode_sample(idxes)
+        return tuple(list(encoded_sample) + [weights, idxes])
+
+    def update_priorities(self, idxes, priorities):
+        """
+        Update priorities of sampled transitions.
+        sets priority of transition at index idxes[i] in buffer
+        to priorities[i].
+        :param idxes: ([int]) List of idxes of sampled transitions
+        :param priorities: ([float]) List of updated priorities corresponding to transitions at the sampled idxes
+            denoted by variable `idxes`.
+        """
+        assert len(idxes) == len(priorities)
+        assert np.min(priorities) > 0
+        assert np.min(idxes) >= 0
+        assert np.max(idxes) < len(self._storage)
+        self._it_sum[idxes] = priorities ** self._alpha
+        self._it_min[idxes] = priorities ** self._alpha
+
+        self._max_priority = max(self._max_priority, np.max(priorities))
 
 
 class ReplayBuffer(object):
--- test_np.py
+++ test_np.py
@@ -7,3 +7,8 @@ d2=np.load("/tmp/d1.npy", allow_pickle=True)
 print(d1['key1'])
 print(d1.get('key1'))
 print(d2.item().get('key2'))
+
+
+array = np.zeros([20])
+for a in array:
+    print(a)
