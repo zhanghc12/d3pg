142f50b853f16fb5a9edd7867f845bec1655e0af
zhanghc <zhanghc12@126.com>
2022-06-20 14:37:45 +0800

    1

--- knn_ue/dppg.py
+++ knn_ue/dppg.py
@@ -71,6 +71,60 @@ class Critic(nn.Module):
         q2 = self.l6(q2)
         return q2
 
+class VAEPolicy():
+    def __init__(
+            self,
+            obs_dim,
+            action_dim,
+            latent_dim,
+    ):
+        self.latent_dim = latent_dim
+
+        self.e1 = torch.nn.Linear(obs_dim + action_dim, 750)
+        self.e2 = torch.nn.Linear(750, 750)
+
+        self.mean = torch.nn.Linear(750, self.latent_dim)
+        self.log_std = torch.nn.Linear(750, self.latent_dim)
+
+        self.d1 = torch.nn.Linear(obs_dim + self.latent_dim, 750)
+        self.d2 = torch.nn.Linear(750, 750)
+        self.d3 = torch.nn.Linear(750, action_dim)
+
+        self.max_action = 1.0
+        self.latent_dim = latent_dim
+
+    def forward(self, state, action):
+        z = F.relu(self.e1(torch.cat([state, action], 1)))
+        z = F.relu(self.e2(z))
+
+        mean = self.mean(z)
+        # Clamped for numerical stability
+        log_std = self.log_std(z).clamp(-4, 15)
+        std = torch.exp(log_std)
+        z = mean + std * torch.from_numpy(np.random.normal(0, 1, size=(std.size()))).to(device)
+
+        u = self.decode(state, z)
+
+        return u, mean, std
+
+    def decode(self, state, z=None):
+        if z is None:
+            z = torch.from_numpy(np.random.normal(0, 1, size=(state.size(0), self.latent_dim))).clamp(-0.5, 0.5).to(device)
+
+        a = F.relu(self.d1(torch.cat([state, z], 1)))
+        a = F.relu(self.d2(a))
+        return torch.tanh(self.d3(a))
+
+
+    def decode_multiple(self, state, z=None, num_decode=10):
+        if z is None:
+            z = torch.from_numpy(np.random.normal(0, 1, size=(state.size(0), num_decode, self.latent_dim))).clamp(-0.5,
+                                                                                                                0.5).to(device)
+
+        a = F.relu(self.d1(torch.cat([state.unsqueeze(0).repeat(num_decode, 1, 1).permute(1, 0, 2), z], 2)))
+        a = F.relu(self.d2(a))
+        return torch.tanh(self.d3(a)), self.d3(a)
+
 
 class TD3(object):
     def __init__(self, state_dim, action_dim, gamma, tau, bc_scale):
@@ -95,6 +149,10 @@ class TD3(object):
         self.critic = Critic(state_dim, action_dim).to(device)
         self.critic_target = copy.deepcopy(self.critic)
         self.critic_optimizer = torch.optim.Adam(self.critic.parameters(), lr=3e-4)
+
+        self.mse_criterion = nn.MSELoss()
+        self.vae = VAEPolicy(state_dim, action_dim, 2 * action_dim).to(device)
+        self.vae_optimizer = torch.optim.Adam(self.vae.parameters(), lr=3e-4)
 
     def select_action(self, state, evaluate=False):
         state = torch.FloatTensor(state).to(self.device).unsqueeze(0)
@@ -150,6 +208,16 @@ class TD3(object):
         priority = 2 - (torch.sqrt(torch.mean((self.actor(next_state) - next_action) ** 2, dim=1))) + 1e-3
         priority = priority.detach().cpu().numpy()
         memory.update_priorities(idxes, priority)
+
+
+        recon, mean, std = self.vae(state, action)
+        recon_loss = self.mse_criterion(recon, action)
+        kl_loss = -0.5 * (1 + torch.log(std.pow(2)) - mean.pow(2) - std.pow(2)).mean()
+        vae_loss = recon_loss + 0.5 * kl_loss
+
+        self.vae_optimizer.zero_grad()
+        vae_loss.backward()
+        self.vae_optimizer.step()
 
         return critic_loss.item(), actor_loss.item()
 
