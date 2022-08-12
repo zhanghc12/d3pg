54da0461fdddd01aed2dd7c50ef7cab77cf423a6
zhanghc <zhanghc12@126.com>
2022-08-12 15:40:26 +0800

    1

--- knn_ue/td3_aaai.py
+++ knn_ue/td3_aaai.py
@@ -259,8 +259,10 @@ class TD3(object):
             target = reward + not_done * self.discount * (sorted_z)
             query_data = self.feature_nn(next_state, new_next_action).detach().cpu().numpy()
             query_data = query_data * phi_std + phi_mean
-            target_distance = kd_trees.query(query_data, k=19)[0] # / (self.state_dim + self.action_dim)
+            target_distance, target_id = kd_trees.query(query_data, k=19)[0] # / (self.state_dim + self.action_dim)
             print(target_distance)
+            print(target_id)
+
             print('---')
             target_distance = np.mean(target_distance, axis=1, keepdims=True)
             cond = -torch.clamp_(self.eta * torch.FloatTensor(target_distance).to(self.device), 0, 1) * 100 + 150
