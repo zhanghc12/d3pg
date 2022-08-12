a383217ef8efe31aa1dc2aa34ba5fa2a34cc9ebb
zhanghc <zhanghc12@126.com>
2022-08-12 16:24:26 +0800

    1

--- aaai_final_8.sh
+++ aaai_final_8.sh
@@ -1,21 +1,21 @@
-CUDA_VISIBLE_DEVICES=0 python offline_kd_aaai.py --policy TQC  --version $1  --bc_scale $2 --env hopper-random-v2 --seed $3 --top_quantiles_to_drop $4 --k $5 &\
-CUDA_VISIBLE_DEVICES=1 python offline_kd_aaai.py --policy TQC  --version $1  --bc_scale $2  --env hopper-medium-v2  --seed $3 --top_quantiles_to_drop $4  --k $5 &\
-CUDA_VISIBLE_DEVICES=2 python offline_kd_aaai.py --policy TQC  --version $1  --bc_scale $2  --env hopper-medium-expert-v2  --seed $3 --top_quantiles_to_drop $4  --k $5 &\
-CUDA_VISIBLE_DEVICES=3 python offline_kd_aaai.py --policy TQC  --version $1  --bc_scale $2  --env hopper-medium-replay-v2 --seed $3 --top_quantiles_to_drop $4 --k $5  &\
-CUDA_VISIBLE_DEVICES=4 python offline_kd_aaai.py --policy TQC  --version $1  --bc_scale $2  --env hopper-expert-v2 --seed $3 --top_quantiles_to_drop $4 --k $5 &\
-CUDA_VISIBLE_DEVICES=5 python offline_kd_aaai.py --policy TQC  --version $1  --bc_scale $2 --env halfcheetah-random-v2 --seed $3 --top_quantiles_to_drop $4 --k $5 &\
-CUDA_VISIBLE_DEVICES=6 python offline_kd_aaai.py --policy TQC  --version $1  --bc_scale $2  --env halfcheetah-medium-v2  --seed $3 --top_quantiles_to_drop $4 --k $5 &\
-CUDA_VISIBLE_DEVICES=7 python offline_kd_aaai.py --policy TQC  --version $1  --bc_scale $2  --env halfcheetah-medium-expert-v2  --seed $3 --top_quantiles_to_drop $4 --k $5 &\
-CUDA_VISIBLE_DEVICES=0 python offline_kd_aaai.py --policy TQC  --version $1  --bc_scale $2  --env halfcheetah-medium-replay-v2 --seed $3 --top_quantiles_to_drop $4 --k $5 &\
-CUDA_VISIBLE_DEVICES=1 python offline_kd_aaai.py --policy TQC  --version $1  --bc_scale $2  --env halfcheetah-expert-v2 --seed $3 --top_quantiles_to_drop $4 --k $5 &\
-CUDA_VISIBLE_DEVICES=2 python offline_kd_aaai.py --policy TQC  --version $1  --bc_scale $2 --env walker2d-random-v2 --seed $3 --top_quantiles_to_drop $4 --k $5 &\
-CUDA_VISIBLE_DEVICES=3 python offline_kd_aaai.py --policy TQC  --version $1  --bc_scale $2  --env walker2d-medium-v2  --seed $3 --top_quantiles_to_drop $4 --k $5 &\
-CUDA_VISIBLE_DEVICES=4 python offline_kd_aaai.py --policy TQC  --version $1  --bc_scale $2  --env walker2d-medium-expert-v2  --seed $3 --top_quantiles_to_drop $4 --k $5 &\
-CUDA_VISIBLE_DEVICES=5 python offline_kd_aaai.py --policy TQC  --version $1  --bc_scale $2  --env walker2d-medium-replay-v2 --seed $3 --top_quantiles_to_drop $4 --k $5 &\
-CUDA_VISIBLE_DEVICES=6 python offline_kd_aaai.py --policy TQC  --version $1  --bc_scale $2  --env walker2d-expert-v2 --seed $3 --top_quantiles_to_drop $4 --k $5
+CUDA_VISIBLE_DEVICES=0 python offline_kd_aaai.py --policy TQC  --version $1  --eta $2 --env hopper-random-v2 --seed $3 --top_quantiles_to_drop $4 --k $5 &\
+CUDA_VISIBLE_DEVICES=1 python offline_kd_aaai.py --policy TQC  --version $1  --eta $2  --env hopper-medium-v2  --seed $3 --top_quantiles_to_drop $4  --k $5 &\
+CUDA_VISIBLE_DEVICES=2 python offline_kd_aaai.py --policy TQC  --version $1  --eta $2  --env hopper-medium-expert-v2  --seed $3 --top_quantiles_to_drop $4  --k $5 &\
+CUDA_VISIBLE_DEVICES=3 python offline_kd_aaai.py --policy TQC  --version $1  --eta $2  --env hopper-medium-replay-v2 --seed $3 --top_quantiles_to_drop $4 --k $5  &\
+CUDA_VISIBLE_DEVICES=4 python offline_kd_aaai.py --policy TQC  --version $1  --eta $2  --env hopper-expert-v2 --seed $3 --top_quantiles_to_drop $4 --k $5 &\
+CUDA_VISIBLE_DEVICES=5 python offline_kd_aaai.py --policy TQC  --version $1  --eta $2 --env halfcheetah-random-v2 --seed $3 --top_quantiles_to_drop $4 --k $5 &\
+CUDA_VISIBLE_DEVICES=6 python offline_kd_aaai.py --policy TQC  --version $1  --eta $2  --env halfcheetah-medium-v2  --seed $3 --top_quantiles_to_drop $4 --k $5 &\
+CUDA_VISIBLE_DEVICES=7 python offline_kd_aaai.py --policy TQC  --version $1  --eta $2  --env halfcheetah-medium-expert-v2  --seed $3 --top_quantiles_to_drop $4 --k $5 &\
+CUDA_VISIBLE_DEVICES=0 python offline_kd_aaai.py --policy TQC  --version $1  --eta $2  --env halfcheetah-medium-replay-v2 --seed $3 --top_quantiles_to_drop $4 --k $5 &\
+CUDA_VISIBLE_DEVICES=1 python offline_kd_aaai.py --policy TQC  --version $1  --eta $2  --env halfcheetah-expert-v2 --seed $3 --top_quantiles_to_drop $4 --k $5 &\
+CUDA_VISIBLE_DEVICES=2 python offline_kd_aaai.py --policy TQC  --version $1  --eta $2 --env walker2d-random-v2 --seed $3 --top_quantiles_to_drop $4 --k $5 &\
+CUDA_VISIBLE_DEVICES=3 python offline_kd_aaai.py --policy TQC  --version $1  --eta $2  --env walker2d-medium-v2  --seed $3 --top_quantiles_to_drop $4 --k $5 &\
+CUDA_VISIBLE_DEVICES=4 python offline_kd_aaai.py --policy TQC  --version $1  --eta $2  --env walker2d-medium-expert-v2  --seed $3 --top_quantiles_to_drop $4 --k $5 &\
+CUDA_VISIBLE_DEVICES=5 python offline_kd_aaai.py --policy TQC  --version $1  --eta $2  --env walker2d-medium-replay-v2 --seed $3 --top_quantiles_to_drop $4 --k $5 &\
+CUDA_VISIBLE_DEVICES=6 python offline_kd_aaai.py --policy TQC  --version $1  --eta $2  --env walker2d-expert-v2 --seed $3 --top_quantiles_to_drop $4 --k $5
 
 
-# CUDA_VISIBLE_DEVICES=6 python offline_kd_aaai.py --policy TQC  --version 3  --bc_scale 20  --env walker2d-expert-v2 --seed 0 --top_quantiles_to_drop 0
+# CUDA_VISIBLE_DEVICES=6 python offline_kd_aaai.py --policy TQC  --version 3  --eta 20  --env walker2d-expert-v2 --seed 0 --top_quantiles_to_drop 0
 
 
 # sh offline_rl_large_scale_single_seed_new.sh 31  1.0 -1 0.2  0.05
